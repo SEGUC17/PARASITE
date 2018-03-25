1cd8a65246ad34349d81c248e7b6cf042a010953
Reda Ramadan <king2sairos2013@gmail.com>
2018-03-25 21:51:33 +0200

    fix: Handle helmet and compression initialization

--- Server/app.js
+++ Server/app.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -5,34 +6,52 @@ var compression = require('compression');
 var bodyParser = require('body-parser');
 var helmet = require('helmet');
 var passport = require('passport');
-var index = require('./api/routes/index');
 
+//config file
+var config = require('./api/config/config');
+//router
+var router = require('./api/routes/index');
+
+//express app
 var app = express();
-app.use(helmet);
-app.use(compression);
+app.set(config.SECRET);
+
+//middleware
+app.use(helmet());
+app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/api', index);
+//router
+app.use('/api', router);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+
+
+// mongoose Database connection
+require('./api/config/DBConnection');
+
+
+
+// 500 internal server error handler
+app.use(function(err, req, res, next) {
+  if (err.statusCode === 404) return next();
+  res.status(500).json({
+    // Never leak the stack trace of the err if running in production mode
+    err: process.env.NODE_ENV === 'production' ? null : err,
+    msg: '500 Internal Server Error',
+    data: null
+  });
 });
 
-// error handler
-app.use(function(err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// 404 error handler
+app.use(function(req, res) {
+  res.status(404).json({
+    err: null,
+    msg: '404 Not Found',
+    data: null
+  });
 });
 
 module.exports = app;
