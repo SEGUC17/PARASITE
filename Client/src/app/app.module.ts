c72c29cc7ea1ad26cb6e50c7d61732075b939bb1
Reda Ramadan <king2sairos2013@gmail.com>
2018-03-28 21:56:16 +0200

    fix: Handle FlexLayout library import issues

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -14,11 +14,11 @@ import { MarketModule } from './market/market.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 import { MediaMatcher } from '@angular/cdk/layout';
-import {MatButtonModule} from '@angular/material/button';
-
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
@@ -32,6 +32,7 @@ import {MatButtonModule} from '@angular/material/button';
     MatSidenavModule,
     MatListModule,
     MatButtonModule,
+    FlexLayoutModule,
     DashboardModule,
     ProfileModule,
     ContentModule,
