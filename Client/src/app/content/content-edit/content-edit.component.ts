0425088a874af6be9214d28565deb3d248eb73ac
Reda Ramadan <king2sairos2013@gmail.com>
2018-03-28 14:26:24 +0200

    feat: Configure basic editor view

--- Client/src/app/content/content-edit/content-edit.component.ts
+++ Client/src/app/content/content-edit/content-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-content-edit',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-edit.component.css']
 })
 export class ContentEditComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  public editor;
+  public content;
+  public editorOut;
+  public trustedOut;
+  public editorContent = `<h1>Hello there, I am Reda :D<h1>`;
+  public editorOptions = {
+    placeholder: 'insert content here'
+  };
+  constructor(private sanitizer: DomSanitizer) {
   }
+  onEditorBlured(quill) {
+    console.log('editor blur!', quill);
+  }
+  onEditorFocused(quill) {
+    console.log('editor focused!', quill);
+  }
+  onEditorCreated(quill) {
+    this.editor = quill;
+    console.log('quill is here', quill);
+  }
+  onContentChanged({ quill, html, text }) {
+    console.log('content changed dude', quill, html, text);
+    this.editorOut = html;
+    this.trustedOut = this.sanitizer.bypassSecurityTrustHtml(this.editorOut);
+  }
+  ngOnInit() {
+    setTimeout(() => {
+      this.editorContent = '<h1>content changed!</h1>';
+      console.log('you can use the quill instance object to do something', this.editor);
+      // this.editor.disable();
+    }, 2800);
 
+
+  }
 }
--- Client/src/app/content/content.module.ts
+++ Client/src/app/content/content.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContentEditComponent } from './content-edit/content-edit.component';
 import { ContentRoutingModule } from './content-routing.module';
+import { FormsModule } from '@angular/forms';
+import { QuillEditorModule } from 'ngx-quill-editor';
 @NgModule({
   imports: [
     CommonModule,
-    ContentRoutingModule
+    ContentRoutingModule,
+    FormsModule,
+    QuillEditorModule
   ],
   declarations: [ContentEditComponent]
 })
