06185e2511e49843897e1b200e53f8b4c6e51dd2
OmarElkilany <omar.alkilani23@gmail.com>
2018-03-29 14:24:27 +0200

    feat: Add initial implmentation of ContentController

---
+++ Server/api/controllers/ContentController.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var Content = mongoose.model('Content');
+
+module.exports.getContentPage = function (req, res, next) {
+    Content.find().exec(function (err, count) {
+        if (err) {
+            return next(err);
+        }
+
+        return res.status(200).json({
+            data: count,
+            err: null,
+            msg: 'productId parameter must be a valid ObjectId.'
+        });
+
+    });
+}
+
+module.exports.getNumberOfContentPages = function (req, res, next) {
+
+}
