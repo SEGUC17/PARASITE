431271e86886096bb56783120b5188aac9d5bc5c
OmarElkilany <omar.alkilani23@gmail.com>
2018-03-29 19:22:43 +0200

    feat and fix: Fix html for content-list, add calls to content-service
    Fix html to reflect changes in content class. Add calls in content-list-view to retrieve the contents from the database.

--- Client/src/app/content/content-list-view/content-list-view.component.ts
+++ Client/src/app/content/content-list-view/content-list-view.component.ts
@@ -10,12 +10,39 @@ import { ContentService } from '../content.service';
 export class ContentListViewComponent implements OnInit {
 
   contents: Content[];
+  currentPageNumber: number;
+  numberOfEntriesPerPage = 10;
+  selectedCategory: String;
+  selectedSection: String;
+  totalNumberOfPages: number;
 
   constructor(private contentService: ContentService ) { }
 
   ngOnInit() {
+    this.currentPageNumber = 1;
+    this.intitializeViewWithFirstPage();
   }
 
   // TODO methods to call service
+  // retrieves the number of content pages and displays the first page
+  intitializeViewWithFirstPage(): void {
+    const self = this;
+    this.contentService.getNumberOfContentPages(self.numberOfEntriesPerPage,
+      self.selectedCategory, self.selectedSection)
+      .subscribe(function (retriedNumberOfPages) {
+        self.totalNumberOfPages = retriedNumberOfPages.data;
+        self.getContentPage();
+      });
+  }
+
+  // retrieves the contents of a particular page according to currentPageNumber
+  getContentPage(): void {
+    const self = this;
+    this.contentService.getContentPage(self.numberOfEntriesPerPage,
+      self.currentPageNumber, self.selectedCategory, self.selectedSection)
+      .subscribe(function (retrievedContents) {
+        self.contents = retrievedContents.data;
+      });
+  }
 
 }
