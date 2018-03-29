5c20a251666158dae9b1793cc4088e3648385614
Heidi <Heidiiashraaf@gmail.com>
2018-03-29 20:15:18 +0200

    feat: Updated routes and Profile controller to add view children feature

--- Server/api/controllers/ProfileController.js
+++ Server/api/controllers/ProfileController.js
@@ -1,6 +1,33 @@
+var mongoose = require('mongoose'),
+User = mongoose.model('User');
+
+
+
 module.exports.requestUserValidation = function(req, res, next) {
 //TODO: make a request
 //Author: Maher
 res.writeHead(200, { 'Content-Type': 'text/plain' });
 res.end('FROM SERVER: making request');
+};
+
+
+module.exports.getChildren = function (req, res, next) {
+   
+    User.findById(req.params.userId).exec(function (err, user) {
+        if (err) {
+            return next(err);
+        }
+
+        if (!user) {
+            return res
+                .status(404)
+                .json({ err: null, msg: 'User not found.', data: null });
+        }
+
+        res.status(200).json({
+            err: null,
+            msg: 'Children retrieved successfully.',
+            data: user.children
+        });
+    });
 };
--- Server/api/routes/index.js
+++ Server/api/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+var profileController = require('../controllers/profile-controller');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('Server Works');
 });
+
+
+
+router.get('/profile/:userId/getChildren', profileController.getProduct);
 
 module.exports = router;
