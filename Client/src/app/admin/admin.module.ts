e9b97816f86b1e3e68b71f53c64201a72f0bce99
Salma <salmaghoneim97@gmail.com>
2018-03-29 13:19:02 +0200

    fix

--- Client/src/app/admin/admin.module.ts
+++ Client/src/app/admin/admin.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminControlComponent } from './admin-control/admin-control.component';
 import { AdminRoutingModule } from './admin-routing.module';
-import { ViewRequestsComponent } from './view-requests/view-requests.component';
 import { ViewResourcesIdeasRequestsComponent } from './view-resources-ideas-requests/view-resources-ideas-requests.component';
 
 @NgModule({
@@ -10,6 +9,6 @@ import { ViewResourcesIdeasRequestsComponent } from './view-resources-ideas-requ
     CommonModule,
     AdminRoutingModule
   ],
-  declarations: [AdminControlComponent, ViewRequestsComponent, ViewResourcesIdeasRequestsComponent]
+  declarations: [AdminControlComponent, ViewResourcesIdeasRequestsComponent]
 })
 export class AdminModule { }
