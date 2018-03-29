f371ce55ae137d833ac3ba8cc9cfe6fb424d1708
Salma <salmaghoneim97@gmail.com>
2018-03-29 13:14:49 +0200

    chore: added viewResourceIdeaRequests

--- Client/src/app/admin/admin.module.ts
+++ Client/src/app/admin/admin.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { AdminControlComponent } from './admin-control/admin-control.component';
 import { AdminRoutingModule } from './admin-routing.module';
 import { ViewRequestsComponent } from './view-requests/view-requests.component';
+import { ViewResourcesIdeasRequestsComponent } from './view-resources-ideas-requests/view-resources-ideas-requests.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AdminRoutingModule
   ],
-  declarations: [AdminControlComponent, ViewRequestsComponent]
+  declarations: [AdminControlComponent, ViewRequestsComponent, ViewResourcesIdeasRequestsComponent]
 })
 export class AdminModule { }
