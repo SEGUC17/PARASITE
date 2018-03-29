d1db7a158f9ad3163caa206787b84277a3942768
JARVIS\Sinclair <exclass9.24@gmail.com>
2018-03-29 13:52:37 +0200

    Merged with branch

---
+++ Client/src/app/profile/profile.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable()
+export class ProfileService {
+// TODO: Write Profile Service AUTHOR: Maher
+}
