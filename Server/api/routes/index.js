24ebebd8d14f0e2adde1dbc9c8d04ab97eccd8fa
EsraaATEFibrahim <esraa.abdelfatah@student.guc.edu.eg>
2018-03-29 12:30:52 +0200

    Feat: added a routing to the backend

--- Server/api/routes/index.js
+++ Server/api/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+productCtrl = require('../controllers/ProductController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('Server Works');
 });
 
+router.post('/productrequest/createproduct', productCtrl.createProduct);
+router.post('/productrequest/createProductRequest', productCtrl.createProductRequest);
+
+// router.get('/productrequest/evaluateRequest', productCtrl.evaluateRequest);
+router.get('/productrequest/getRequests', productCtrl.getRequests);
+
 module.exports = router;
+
