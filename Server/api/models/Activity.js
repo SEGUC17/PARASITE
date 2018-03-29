7022b3fc1472e52e3290f45fd6b7e2086b161f3d
Wessam Abdelbary Ali <wessam611@live.com>
2018-03-29 14:15:31 +0200

    fix: Added price and status to Activity and validating from/toDate

--- Server/api/models/Activity.js
+++ Server/api/models/Activity.js
@@ -26,24 +26,30 @@ var activitySchema = Schema({
             { type: Schema.Types.ObjectId, ref: 'User' }
         ]
     },
+    price: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['pending', 'rejected', 'verified']
+    },
     fromDateTime: {
         type: Number,
         required: true,
-        validate: {
-            validator: function (time) {
-                return isTimestamp(time);
-            }
-        }
+        validate: 
+            [isTimestamp, 'Date has to be in unix format']
+        
     },
     toDateTime: {
         type: Number,
         required: true,
-        validate: {
-            validator: function (time) {
-                return isTimestamp(time);
-            }
-        }
-    }
+        validate: [function(time){
+            // Making sure that fromDate is less than toDate
+            return isTimestamp(time) && this.fromDateTime <= time;
+        }, 'fromDate has to be less than toDate']
+   }    
 });
 
 // Adds CreatedAt, UpdatedAt fields in Unix format
