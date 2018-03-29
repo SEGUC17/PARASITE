90c8cb5ef23d97b66ea9af841c3cca8348a08388
mariamkhmahran <mariamkhmahran@hotmail.com>
2018-03-29 20:00:03 +0200

    feat: added the 'add-psychologist-request' component

---
+++ Client/src/app/psychologist/add-psych-request/AddPsychologistRequest.ts
@@ -0,0 +1,13 @@
+
+export class AddPsychologistRequest {
+  
+  firstName: string;
+  lastName: string;
+  phone: number;
+  address: string;
+  email: string;
+  daysOff: string[];
+  priceRange: number;
+  state: string;
+
+};
