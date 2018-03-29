497e6eaf8576875d86b4deacfc82990722f77f04
Heidi <Heidiiashraaf@gmail.com>
2018-03-29 21:01:11 +0200

    fix: fixed conflicts

--- Client/src/app/profile/profile.module.ts
+++ Client/src/app/profile/profile.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile/profile.component';
+<<<<<<< HEAD
 import { ChildernComponent } from './profile/childern/childern.component';
+=======
+>>>>>>> 0f9635eb3ec30579faf013954906dd25c43f5ea1
 
 @NgModule({
   imports: [
     ProfileRoutingModule
   ],
+<<<<<<< HEAD
   declarations: [ProfileComponent, ChildernComponent]
+=======
+  declarations: [ProfileComponent]
+>>>>>>> 0f9635eb3ec30579faf013954906dd25c43f5ea1
 })
 export class ProfileModule { }
