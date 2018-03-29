844c22fc0010cf806024e2a3c28eaa0e3733fcf8
Salma <salmaghoneim97@gmail.com>
2018-03-29 12:38:33 +0200

    added admin service and a components for view-requests

--- Client/src/app/admin/admin.module.ts
+++ Client/src/app/admin/admin.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminControlComponent } from './admin-control/admin-control.component';
 import { AdminRoutingModule } from './admin-routing.module';
+import { ViewRequestsComponent } from './view-requests/view-requests.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AdminRoutingModule
   ],
-  declarations: [AdminControlComponent]
+  declarations: [AdminControlComponent, ViewRequestsComponent]
 })
 export class AdminModule { }
