c1f1d67560d01adeaeb0769b5b7aca02163eddf9
Reda Ramadan <king2sairos2013@gmail.com>
2018-03-29 15:16:16 +0200

    style: Add steppers to content-edit

--- Client/src/app/content/content.module.ts
+++ Client/src/app/content/content.module.ts
@@ -5,12 +5,20 @@ import { ContentRoutingModule } from './content-routing.module';
 import { FormsModule } from '@angular/forms';
 import { QuillEditorModule } from 'ngx-quill-editor';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
 @NgModule({
   imports: [
     CommonModule,
     ContentRoutingModule,
     FlexLayoutModule,
     FormsModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
     QuillEditorModule
   ],
   declarations: [ContentEditComponent]
