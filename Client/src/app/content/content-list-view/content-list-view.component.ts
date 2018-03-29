c39aa70354ba0948e3f08367b015c73ee1b4cad5
OmarElkilany <omar.alkilani23@gmail.com>
2018-03-29 14:59:25 +0200

    fix: Add category, section, tags to Content schema

--- Client/src/app/content/content-list-view/content-list-view.component.ts
+++ Client/src/app/content/content-list-view/content-list-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Content} from '../content';
+import { Content } from '../content';
 
 @Component({
   selector: 'app-content-list-view',
@@ -8,7 +8,7 @@ import {Content} from '../content';
 })
 export class ContentListViewComponent implements OnInit {
 
-  contents: Content[] = [ {
+  contents: Content[] = [{
     id: '198387492',
     title: 'Best Resource for Learning C#',
     contentImage: 'assets/images/content-view/contentimagetest.jpg',
@@ -17,69 +17,69 @@ export class ContentListViewComponent implements OnInit {
     tags: ['programming', 'beginner', 'C#', 'clickbait', 'life', 'mood', 'fatality', 'engineering concepts', 'computer science']
   },
   {
-  id: '198387492',
-  title: 'Best Resource for Learning C#',
-  contentImage: 'assets/images/content-view/contentimagetest.jpg',
-  creatorUsername: 'OmarK', creatorProfileLink: 'http://mywebsite.com/OmarK',
-  creatorAvatar: 'assets/images/content-view/profiletest.jpg',
-  tags: ['programming', 'beginner', 'C#', 'clickbait', 'life', 'mood', 'fatality', 'engineering concepts', 'computer science']
-},
-{
-id: '198387492',
-title: 'Best Resource for Learning C#',
-contentImage: 'assets/images/content-view/contentimagetest.jpg',
-creatorUsername: 'OmarK', creatorProfileLink: 'http://mywebsite.com/OmarK',
-creatorAvatar: 'assets/images/content-view/profiletest.jpg',
-tags: ['programming', 'beginner', 'C#', 'clickbait', 'life', 'mood', 'fatality', 'engineering concepts', 'computer science']
-},
-{
-id: '198387492',
-title: 'Best Resource for Learning C#',
-contentImage: 'assets/images/content-view/contentimagetest.jpg',
-creatorUsername: 'OmarK', creatorProfileLink: 'http://mywebsite.com/OmarK',
-creatorAvatar: 'assets/images/content-view/profiletest.jpg',
-tags: ['programming', 'beginner', 'C#', 'clickbait', 'life', 'mood', 'fatality', 'engineering concepts', 'computer science']
-},
-{
-id: '198387492',
-title: 'Best Resource for Learning C#',
-contentImage: 'assets/images/content-view/contentimagetest.jpg',
-creatorUsername: 'OmarK', creatorProfileLink: 'http://mywebsite.com/OmarK',
-creatorAvatar: 'assets/images/content-view/profiletest.jpg',
-tags: ['programming', 'beginner', 'C#', 'clickbait', 'life', 'mood', 'fatality', 'engineering concepts', 'computer science']
-},
-{
-id: '198387492',
-title: 'Best Resource for Learning C#',
-contentImage: 'assets/images/content-view/contentimagetest.jpg',
-creatorUsername: 'OmarK', creatorProfileLink: 'http://mywebsite.com/OmarK',
-creatorAvatar: 'assets/images/content-view/profiletest.jpg',
-tags: ['programming', 'beginner', 'C#', 'clickbait', 'life', 'mood', 'fatality', 'engineering concepts', 'computer science']
-},
-{
-id: '198387492',
-title: 'Best Resource for Learning C#',
-contentImage: 'assets/images/content-view/contentimagetest.jpg',
-creatorUsername: 'OmarK', creatorProfileLink: 'http://mywebsite.com/OmarK',
-creatorAvatar: 'assets/images/content-view/profiletest.jpg',
-tags: ['programming', 'beginner', 'C#', 'clickbait', 'life', 'mood', 'fatality', 'engineering concepts', 'computer science']
-},
-{
-id: '198387492',
-title: 'Best Resource for Learning C#',
-contentImage: 'assets/images/content-view/contentimagetest.jpg',
-creatorUsername: 'OmarK', creatorProfileLink: 'http://mywebsite.com/OmarK',
-creatorAvatar: 'assets/images/content-view/profiletest.jpg',
-tags: ['programming', 'beginner', 'C#', 'clickbait', 'life', 'mood', 'fatality', 'engineering concepts', 'computer science']
-},
-{
-id: '198387492',
-title: 'Best Resource for Learning C#',
-contentImage: 'assets/images/content-view/contentimagetest.jpg',
-creatorUsername: 'OmarK', creatorProfileLink: 'http://mywebsite.com/OmarK',
-creatorAvatar: 'assets/images/content-view/profiletest.jpg',
-tags: ['programming', 'beginner', 'C#', 'clickbait', 'life', 'mood', 'fatality', 'engineering concepts', 'computer science']
-}];
+    id: '198387492',
+    title: 'Best Resource for Learning C#',
+    contentImage: 'assets/images/content-view/contentimagetest.jpg',
+    creatorUsername: 'OmarK', creatorProfileLink: 'http://mywebsite.com/OmarK',
+    creatorAvatar: 'assets/images/content-view/profiletest.jpg',
+    tags: ['programming', 'beginner', 'C#', 'clickbait', 'life', 'mood', 'fatality', 'engineering concepts', 'computer science']
+  },
+  {
+    id: '198387492',
+    title: 'Best Resource for Learning C#',
+    contentImage: 'assets/images/content-view/contentimagetest.jpg',
+    creatorUsername: 'OmarK', creatorProfileLink: 'http://mywebsite.com/OmarK',
+    creatorAvatar: 'assets/images/content-view/profiletest.jpg',
+    tags: ['programming', 'beginner', 'C#', 'clickbait', 'life', 'mood', 'fatality', 'engineering concepts', 'computer science']
+  },
+  {
+    id: '198387492',
+    title: 'Best Resource for Learning C#',
+    contentImage: 'assets/images/content-view/contentimagetest.jpg',
+    creatorUsername: 'OmarK', creatorProfileLink: 'http://mywebsite.com/OmarK',
+    creatorAvatar: 'assets/images/content-view/profiletest.jpg',
+    tags: ['programming', 'beginner', 'C#', 'clickbait', 'life', 'mood', 'fatality', 'engineering concepts', 'computer science']
+  },
+  {
+    id: '198387492',
+    title: 'Best Resource for Learning C#',
+    contentImage: 'assets/images/content-view/contentimagetest.jpg',
+    creatorUsername: 'OmarK', creatorProfileLink: 'http://mywebsite.com/OmarK',
+    creatorAvatar: 'assets/images/content-view/profiletest.jpg',
+    tags: ['programming', 'beginner', 'C#', 'clickbait', 'life', 'mood', 'fatality', 'engineering concepts', 'computer science']
+  },
+  {
+    id: '198387492',
+    title: 'Best Resource for Learning C#',
+    contentImage: 'assets/images/content-view/contentimagetest.jpg',
+    creatorUsername: 'OmarK', creatorProfileLink: 'http://mywebsite.com/OmarK',
+    creatorAvatar: 'assets/images/content-view/profiletest.jpg',
+    tags: ['programming', 'beginner', 'C#', 'clickbait', 'life', 'mood', 'fatality', 'engineering concepts', 'computer science']
+  },
+  {
+    id: '198387492',
+    title: 'Best Resource for Learning C#',
+    contentImage: 'assets/images/content-view/contentimagetest.jpg',
+    creatorUsername: 'OmarK', creatorProfileLink: 'http://mywebsite.com/OmarK',
+    creatorAvatar: 'assets/images/content-view/profiletest.jpg',
+    tags: ['programming', 'beginner', 'C#', 'clickbait', 'life', 'mood', 'fatality', 'engineering concepts', 'computer science']
+  },
+  {
+    id: '198387492',
+    title: 'Best Resource for Learning C#',
+    contentImage: 'assets/images/content-view/contentimagetest.jpg',
+    creatorUsername: 'OmarK', creatorProfileLink: 'http://mywebsite.com/OmarK',
+    creatorAvatar: 'assets/images/content-view/profiletest.jpg',
+    tags: ['programming', 'beginner', 'C#', 'clickbait', 'life', 'mood', 'fatality', 'engineering concepts', 'computer science']
+  },
+  {
+    id: '198387492',
+    title: 'Best Resource for Learning C#',
+    contentImage: 'assets/images/content-view/contentimagetest.jpg',
+    creatorUsername: 'OmarK', creatorProfileLink: 'http://mywebsite.com/OmarK',
+    creatorAvatar: 'assets/images/content-view/profiletest.jpg',
+    tags: ['programming', 'beginner', 'C#', 'clickbait', 'life', 'mood', 'fatality', 'engineering concepts', 'computer science']
+  }];
 
   constructor() { }
 
