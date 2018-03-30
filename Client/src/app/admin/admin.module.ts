d5662270a2495f21353bff9e90d2059feebd4c63
JARVIS\Sinclair <exclass9.24@gmail.com>
2018-03-30 20:41:40 +0200

    feat: frontend for products requests with mock data done.

--- Client/src/app/admin/admin.module.ts
+++ Client/src/app/admin/admin.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { AdminControlComponent } from './admin-control/admin-control.component';
 import { AdminRoutingModule } from './admin-routing.module';
 import { ViewProductRequestsComponent } from './view-product-requests/view-product-requests.component';
+import { MatCardModule } from '@angular/material/card';
+import { ProductRequestsService } from './view-product-requests/product-requests.service';
 
 @NgModule({
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    MatCardModule
   ],
-  declarations: [AdminControlComponent, ViewProductRequestsComponent]
+  declarations: [AdminControlComponent, ViewProductRequestsComponent],
+  providers: [ProductRequestsService]
 })
 export class AdminModule { }
--- Client/src/app/admin/view-product-requests/view-product-requests.component.ts
+++ Client/src/app/admin/view-product-requests/view-product-requests.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductRequestsService } from './product-requests.service';
-import {MatCardModule} from '@angular/material/card';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-view-product-requests',
@@ -9,13 +9,41 @@ import {MatCardModule} from '@angular/material/card';
 })
 export class ViewProductRequestsComponent implements OnInit {
 
+  mockRequests = [{
+    name: 'Dildo',
+    price: 4.50,
+    seller: 'Abbas Pasha',
+    image: null,
+    acquiringType: 'Rent',
+    rentPeriod: 130,
+    description: 'A dildo for all your sexual needs'
+  },
+  {
+    name: 'Big weird toy',
+    price: 5.60,
+    seller: 'Mamdouh Beh',
+    image: null,
+    acquiringType: 'Sale',
+    rentPeriod: 0,
+    description: 'A big toy your sexual needs'
+  }
+  ];
+
   requests: any[];
 
   constructor(private services: ProductRequestsService) {
     // TODO send request to database to get products
-   }
+  }
 
   ngOnInit() {
+  }
+
+  acceptReq(index) {
+    console.log('Accepted ' + index);
+  }
+
+  rejectReq(index) {
+    console.log('Rejected ' + index);
   }
 
 }
