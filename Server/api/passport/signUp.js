7b6af5c5b03f039c4b820b5acca1225abb1aa848
Maqarios <maqarioswagdy@gmail.com>
2018-03-30 21:51:08 +0200

    feat: add type check to sign up passport

--- Server/api/passport/signUp.js
+++ Server/api/passport/signUp.js
@@ -16,7 +16,7 @@ var newUser = new User();
 // ---------------------- Validators ---------------------- //
 var isArray = require('../utils/validators/is-array');
 var isBoolean = require('../utils/validators/is-boolean');
-var isDateInFuture = require('../utils/validators/is-date-in-future');
+var isDate = require('../utils/validators/is-date');
 var isEmpty = require('../utils/validators/is-empty');
 var isInteger = require('../utils/validators/is-integer');
 var isString = require('../utils/validators/is-string');
@@ -41,9 +41,24 @@ module.exports = function (passport) {
                 newUser.phone = req.body.phone;
                 newUser.username = req.body.username;
 
+                // --- Type Check --- //
+                try {
+                    isString.isString(newUser.address);
+                    isDate.isDate(newUser.birthdate);
+                    isArray.isArray(newUser.children);
+                    isString.isString(newUser.email);
+                    isBoolean.isBoolean(newUser.isChild);
+                    isBoolean.isBoolean(newUser.isParent);
+                    isBoolean.isBoolean(newUser.isTeacher);
+                    isString.isString(newUser.password);
+                    isArray.isArray(newUser.phone);
+                    isString.isString(newUser.username);
+                } catch(e) {
+                    return done(null, false, {"signUpMessage": e});
+                }
+                // --- End of Type Check --- //
 
-
-
+                
 
 
 
@@ -63,9 +78,9 @@ module.exports = function (passport) {
                     }
                 });
 
-                newUser.save(function (err2) {
-                    if (err2) {
-                        throw err2;
+                newUser.save(function (err) {
+                    if (err) {
+                        throw err;
                     }
 
                     return done(null, newUser);
