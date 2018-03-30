d72eacd1e4da6d385aa5b0c5557338c5224f2556
Wessam Abdelbary Ali <wessam611@live.com>
2018-03-30 16:57:03 +0200

    Fix: Added catching error to get request in front-end

--- Client/src/app/activities/activity.service.ts
+++ Client/src/app/activities/activity.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { catchError, tap } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
 
 import { apiUrl } from '../variables';
 
@@ -21,7 +22,19 @@ export class ActivityService {
 
   getActivities(page): Observable<any> {
     // Getting all activities
-    return this.http.get<any>(`${this.activitiesUrl}?page=${page}`).pipe( );
+    return this.http.get<any>(`${this.activitiesUrl}?page=${page}`).pipe(
+      catchError(this.handleError('getNumberOfContentPages', []))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return function (error: any): Observable<T> {
+
+      console.error(error); // log to console instead
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
 }
