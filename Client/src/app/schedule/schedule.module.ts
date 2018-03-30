fbc7c6a63c386726c3afce57b5e361c7a6e82ab3
OmarOES <omar.ohm.elsherif@gmail.com>
2018-03-30 17:15:49 +0200

    feat: added edit tools to study plan page
    Added edit tools for testing purposes.

--- Client/src/app/schedule/schedule.module.ts
+++ Client/src/app/schedule/schedule.module.ts
@@ -10,6 +10,13 @@ import { CalendarModule } from 'angular-calendar';
 import { CalendarHeaderComponent } from './calendar-header/calendar-header.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { DateTimePickerComponent } from './date-time-picker/date-time-picker.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbDatepickerModule,
+  NgbTimepickerModule
+} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
@@ -17,9 +24,13 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserAnimationsModule,
     CalendarModule.forRoot(),
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule,
+    NgbModule,
+    NgbDatepickerModule.forRoot(),
+    NgbTimepickerModule.forRoot()
   ],
-  declarations: [ScheduleComponent, StudyPlanComponent, CalendarHeaderComponent],
+  declarations: [ScheduleComponent, StudyPlanComponent, CalendarHeaderComponent, DateTimePickerComponent],
   providers: [StudyPlanService, ScheduleService]
 })
 export class ScheduleModule { }
