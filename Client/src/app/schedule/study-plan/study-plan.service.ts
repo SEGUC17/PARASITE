a909d3757f10bff271aa53f5ed6c7b5064909420
OmarOES <omar.ohm.elsherif@gmail.com>
2018-03-30 21:08:17 +0200

    feat: added methods in the stud plan service

--- Client/src/app/schedule/study-plan/study-plan.service.ts
+++ Client/src/app/schedule/study-plan/study-plan.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { StudyPlan } from './study-plan';
 
 @Injectable()
 export class StudyPlanService {
@@ -7,5 +9,21 @@ export class StudyPlanService {
   endpoint: String = 'http://localhost:3000/api/';
 
   constructor(private http: HttpClient) { }
+
+  getPerosnalStudyPlans(username: String): Observable<any> {
+    return this.http.get(this.endpoint + 'study-plan/getPerosnalStudyPlans/' + username);
+  }
+
+  getPersonalStudyPlan(username: String, studyPlanID: String): Observable<any> {
+    return this.http.get(this.endpoint + 'study-plan/getPerosnalStudyPlan/' + username + '/' + studyPlanID);
+  }
+
+  getPublishedStudyPlan(studyPlanID: String): Observable<any> {
+    return this.http.get(this.endpoint + 'study-plan/getPublishedStudyPlan/' + studyPlanID);
+  }
+
+  createStudyPlan(username: String, studyPlan: StudyPlan): Observable<any> {
+    return this.http.post(this.endpoint + 'study-plan/createStudyPlan/' + username, studyPlan);
+  }
 
 }
