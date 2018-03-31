076a0a8046cb453d2e66209a29c8fa9b687ac82b
Reda Ramadan <king2sairos2013@gmail.com>
2018-03-31 11:45:21 +0200

    feat: Handle category management logic

--- Server/api/controllers/ContentController.js
+++ Server/api/controllers/ContentController.js
@@ -1,7 +1,6 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose').set('debug', true);
 var Content = mongoose.model('Content');
-
-
+var Category = mongoose.model('Category');
 module.exports.getNumberOfContentPages = function (req, res, next) {
     if (req.params.category !== 'NoCat' &&
         req.params.section !== 'NoSec') {
@@ -113,33 +112,6 @@ module.exports.getContentPage = function (req, res, next) {
     }
 };
 
-// TODO: manage permissions specific behavior for content creation
-module.exports.createContent = function (req, res, next) {
-    var valid = req.body.title &&
-        req.body.body &&
-        req.body.category &&
-        req.body.section;
-    if (!valid) {
-        return res.status.json({
-            data: null,
-            err: 'content metadata not supplied',
-            msg: null
-        });
-    }
-    delete req.body.touchDate;
-    Content.create(req.body, function (err, content) {
-        if (err) {
-            return next(err);
-        }
-        res.status(201).json({
-            data: content,
-            err: null,
-            msg: 'Content was created successfully'
-        });
-    });
-
-
-};
 
 module.exports.getContentById = function (req, res, next) {
 
@@ -227,4 +199,120 @@ module.exports.getNumberOfContentByCreator = function (req, res, next) {
                 msg: 'Number of content by creator retrieved successfully'
             });
         });
+};
+
+// TODO: manage permissions specific behavior for content creation
+module.exports.createContent = function (req, res, next) {
+    var valid = req.body.title &&
+        req.body.body &&
+        req.body.category &&
+        req.body.section;
+    if (!valid) {
+        return res.status.json({
+            data: null,
+            err: 'content metadata not supplied',
+            msg: null
+        });
+    }
+    delete req.body.touchDate;
+    Content.create(req.body, function (err, content) {
+        if (err) {
+            return next(err);
+        }
+        res.status(201).json({
+            data: content,
+            err: null,
+            msg: 'Content was created successfully'
+        });
+    });
+
+
+};
+
+module.exports.getCategories = function (req, res, next) {
+    Category.find({}).exec(function (err, categories) {
+        if (err) {
+            return next(err);
+        }
+
+        return res.status(200).json({
+            data: categories,
+            err: null,
+            msg: 'retrieved catogies successfully'
+        });
+    });
+
+};
+
+module.exports.createCategory = function (req, res, next) {
+    if (!req.body.category) {
+        return res.status(422).json({
+            data: null,
+            err: 'No category supplied',
+            msg: null
+        });
+
+    }
+
+    if (!req.body.category) {
+        return res.status(422).json({
+            data: null,
+            err: 'Wrong Category type error',
+            msg: null
+        });
+    }
+
+    Category.create(
+        { name: req.body.category.trim().toLowerCase() },
+        function (err, category) {
+            if (err) {
+                return next(err);
+            }
+
+            return res.status(201).json({
+                data: category,
+                err: null,
+                msg: 'Category was created successfully'
+            });
+        }
+    );
+
+};
+
+module.exports.createSection = function (req, res, next) {
+    if (!req.params.id) {
+        return res.status(422).json({
+            data: null,
+            err: 'The category is not supplied',
+            msg: null
+        });
+    }
+
+    if (!req.body.section) {
+        return res.status(422).json({
+            data: null,
+            err: 'The section is not supplied',
+            msg: null
+        });
+    }
+    Category.findByIdAndUpdate(
+        req.params.id,
+        { $push: { sections: { name: req.body.section } } },
+        { new: true },
+        function (updateError, updatedCategory) {
+            if (updateError) {
+                console.log(updateError);
+
+                return next(updateError);
+            }
+
+            return res.status(200).json({
+                data: updatedCategory,
+                err: null,
+                msg: 'Updated the category with the section ' +
+                    req.body.section + ' successfully!'
+            });
+        }
+    );
+
 };
