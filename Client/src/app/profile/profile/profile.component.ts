6eee198c2796fda32964cca28117b494bd40b67c
Ahmed Hisham Rihan <ahmed11rihan@gmail.com>
2018-04-02 17:17:07 +0200

    Added the Authentication service to the profile component

--- Client/src/app/profile/profile/profile.component.ts
+++ Client/src/app/profile/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../profile.service';
+import { AuthService} from '../../auth/auth.service';
 
 @Component({
   selector: 'app-profile',
