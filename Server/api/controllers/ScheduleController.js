665f7c10e0a13f1ef45356bfe2815f57af8a9096
--unset <omarelsebai3@yahoo.com>
2018-04-02 23:26:54 +0200

    feat: added error in response for when a user is somehow no longer/never was in database

--- Server/api/controllers/ScheduleController.js
+++ Server/api/controllers/ScheduleController.js
@@ -16,20 +16,28 @@ module.exports.getPersonalSchedule = function (req, res, next) {
     });
 };
 
-    module.exports.updateSchedule = function (req, res, next) {
-        console.log('Entered Service');
-        User.findOneAndUpdate(
-            { username: req.params.username },
-             { $set: { 'schedule': req.body } },
-         function (err, user) {
-            if (err) {
-                return next(err);
-            }
-            res.status(200).json({
-                data: user.schedule,
-                err: null,
-                msg: 'Schedule updated succesfully.'
-            });
+module.exports.updateSchedule = function (req, res, next) {
+    console.log('Entered Service');
+    User.findOneAndUpdate(
+        { username: req.params.username },
+         { $set: { 'schedule': req.body } },
+     function (err, user) {
+        if (err) {
+            return next(err);
         }
-    );
+        if (!user) {
+        return res.status(404).json({
+            data: null,
+            err: 'User not found.',
+            msg: null
+        });
+    }
+        
+        return res.status(200).json({
+            data: user.schedule,
+            err: null,
+            msg: 'Schedule updated succesfully.'
+        });
+    }
+);
 };
