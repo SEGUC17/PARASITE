b83f47c7dc7ce378a44edae6b26bf72a8d8c1c3a
Maqarios <maqarioswagdy@gmail.com>
2018-04-03 21:08:00 +0200

    feat: finish server side authentication

--- Server/api/config/passport.js
+++ Server/api/config/passport.js
@@ -28,3 +28,16 @@ module.exports = function (passport) {
         }
     ));
 };
+
+module.exports.getToken = function (headers) {
+    if (headers && headers.authorization) {
+        var parted = headers.authorization.split(' ');
+        if (parted.length === 2) {
+            return parted[1];
+        }
+
+        return null;
+    }
+
+    return null;
+};
