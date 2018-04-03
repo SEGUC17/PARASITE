5fd14bbd18afc0b5e04b2cd88fed563326956727
Maqarios <maqarioswagdy@gmail.com>
2018-04-03 13:17:20 +0200

    feat: add hashing/comparing password to User Schema Itself

--- Server/api/models/User.js
+++ Server/api/models/User.js
@@ -1,19 +1,25 @@
-// ---------------------- Comments ---------------------- //
+/* eslint-disable consistent-this */
+// -------------------------- Comments ----------------------------------- //
 // Check The Following Website For All Schema Types Supported By Mongoose
 // http://mongoosejs.com/docs/2.7.x/docs/schematypes.html
-// ---------------------- End of Comments ---------------------- //
+// -------------------------- End of "Comments" -------------------------- //
 
 
-// ---------------------- Reuirements ---------------------- //
+// -------------------------- Reuirements -------------------------------- //
 require('./CalendarEvent.js');
 require('./StudyPlan.js');
 var mongoose = require('mongoose');
-var calendarEventSchema = mongoose.model('CalendarEvent').schema,
-    studyPlanSchema = mongoose.model('StudyPlan').schema;
-// ---------------------- End of Requiremenets ---------------------- //
+var encryption = require('../utils/encryption/encryption');
+// -------------------------- End of "Requiremenets" --------------------- //
 
 
-// ---------------------- Schemas ---------------------- //
+// -------------------------- Variables Dependancies --------------------- //
+var calendarEventSchema = mongoose.model('CalendarEvent').schema;
+var studyPlanSchema = mongoose.model('StudyPlan').schema;
+// -------------------------- End of "Variables Dependancies" ------------ //
+
+
+// -------------------------- Schemas ------------------------------------ //
 var userSchema = mongoose.Schema({
     address: {
         index: true,
@@ -98,9 +104,43 @@ var userSchema = mongoose.Schema({
         type: Boolean
     }
 });
-// ---------------------- End of Schemas ---------------------- //
+// -------------------------- End of "Schemas" --------------------------- //
 
 
-// ---------------------- Models ---------------------- //
-var User = mongoose.model('User', userSchema);
-// ---------------------- End of Models ---------------------- //
+// -------------------------- Hash Password ------------------------------ //
+userSchema.pre('save', function (next) {
+    var user = this;
+    if (this.isModified('password' || this.isNew)) {
+        encryption.hashPassword(user.password, function (err, hash) {
+            if (err) {
+                return next(err);
+            }
+
+            user.password = hash;
+
+            return next();
+        });
+    }
+});
+// -------------------------- End of "Hash Password" --------------------- //
+
+
+// -------------------------- Compare Password --------------------------- //
+userSchema.methods.comparePasswords = function (password, next) {
+    encryption.comparePasswordToHash(password, this.password, function (
+        err,
+        passwordMatches
+    ) {
+        if (err) {
+            return next(err);
+        }
+
+        return next(null, passwordMatches);
+    });
+};
+// -------------------------- End of "Compare Password" ------------------ //
+
+
+// -------------------------- Models ------------------------------------- //
+module.exports = mongoose.model('User', userSchema);
+// -------------------------- End of "Models" ---------------------------- //
