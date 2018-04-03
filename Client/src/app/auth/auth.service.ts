6be76dc52d45b8291c04b03e14d896a3ecd5503b
Maqarios <maqarioswagdy@gmail.com>
2018-04-03 10:40:06 +0200

    feat: messages appear on laert message

--- Client/src/app/auth/auth.service.ts
+++ Client/src/app/auth/auth.service.ts
@@ -11,20 +11,31 @@ export class AuthService {
   user: any = {};
   authHeader: Headers = new Headers();
 
-  signUp(user: any): Observable<any>{
-    // console.log(user);
-    // this.signUp(user).subscribe(function(res){});
-    return this.http.post<any>("http://localhost:3000/api/signup",user);
-          
+  signUp(user: any): Observable<any> {
+    const self = this;
+    return this.http.post<any>("http://localhost:3000/api/signup", user).pipe(
+      catchError(self.handleError('signUp', [])));
+
   }//end method
 
 
-  Login(user: any) : Observable<any>{
-    // console.log(user);
-    // this.Login(user).subscribe(function(res){});
-    return this.http.post<any>("http://localhost:3000/api/signin", user);
+  Login(user: any): Observable<any> {
+    const self = this;
+    return this.http.post<any>("http://localhost:3000/api/signin", user).pipe(
+      catchError(self.handleError('Login', [])));
 
-    }//end method
+  }//end method
+
+  private handleError<T>(operation = 'operation', result?: T) {
+
+    return function (error: any): Observable<T> {
+
+      alert(error.error.msg);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
   setUser(user: any): void {
     this.user = user;
   }
