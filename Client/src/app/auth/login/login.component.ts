84380e2390ebe5700f0b850dfca68f0ffca44227
Reda Ramadan <king2sairos2013@gmail.com>
2018-04-06 17:16:31 +0200

    feat: Change login component logic

--- Client/src/app/auth/login/login.component.ts
+++ Client/src/app/auth/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { AuthService } from '../auth.service';
-
+import { User } from '../user';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,12 +9,11 @@ import { AuthService } from '../auth.service';
 })
 export class LoginComponent implements OnInit {
 
-
-  Username: String = "";
-  Password: String = "";
+  public user: User = {
+    username: '',
+    password: ''
+  };
   User: any;
-  Div1 = false;
-  Div2 = false;
   AllisWell: Boolean = true;
 
 
@@ -24,35 +23,22 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-    var self = this;
-    if (this.Password == "" || this.Username == "") {
-      this.showDiv1();
-      console.log("Sorry, you need to enter your username and password to login!");
-      this.AllisWell = false;
-    }
-
-    if (this.AllisWell == true) {
-      this.User = { 'username': this.Username, 'password': this.Password };
-      self.authService.signIn(this.User).subscribe(function (res) {
-        self.authService.setToken(res.token);
-        if (res.data) {
-          alert(res.msg);
-        }
-      });
-    }
-    //self.location.back();
-  }//end method
+    const self = this;
+    self.authService.signIn(this.User).subscribe(function (res) {
+      self.authService.setToken(res.token);
+      if (res.data) {
+        alert(res.msg);
+        self.location.back();
+      }
+    });
+  }
 
 
 
 
-  //this method redirects the user back to the last page he was on before signing up
+  // this method redirects the user back to the last page he was on before signing up
   redirect() {
     this.location.back();
-  }//end method
-
-  showDiv1() {
-    this.Div1 = true;
-  }
+  }// end method
 
 }
