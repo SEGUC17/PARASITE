ad3b32dd3cd077b77d7d42b52a0783a7b1fa0388
mariamkhmahran <mariamkhmahran@hotmail.com>
2018-04-07 23:27:36 +0200

    added test module for testing psychologist's request for adding info

---
+++ Server/tests/psychologists/psychRequest.spec.js
@@ -0,0 +1,35 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../../app');
+var should = chai.should();
+var request = require('supertest');
+var assert = chai.assert;
+
+chai.use(chaiHttp);
+
+describe('send request to add psychologist information', function() {
+    it('post a request to add psychologist information', function() {
+        var req = {
+            address: 'here',
+            createdAt: '1/1/2018',
+            daysOff:
+            [
+                'sat',
+                'sun'
+            ],
+            email: 'blah@blah.com',
+            firstName: 'mariam',
+            lastName: 'mahran',
+            phone: '010101',
+            priceRange: 1000
+        };
+        chai.request(server).post('/api/psychologist/request/add/addRequest').
+        send(req).
+        end(function(err, res) {
+            if (err) {
+            return console.log(err);
+            }
+            res.should.have.status(200);
+        });
+    });
+});
