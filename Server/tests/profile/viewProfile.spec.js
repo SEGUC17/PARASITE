7ebf44bf3d7dde702bb60271daea0f01f737eb7b
Ahmed Hisham Rihan <ahmed11rihan@gmail.com>
2018-04-07 20:11:16 +0200

    update: added test for child adding someone as a parent

--- Server/tests/profile/viewProfile.spec.js
+++ Server/tests/profile/viewProfile.spec.js
@@ -1,23 +1,54 @@
-var assert = require('assert');
-var should = require('chai').should();
-var expect = require('chai').expect();
-var request = require('request');
-var app = require('../../app');
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../../app');
+var should = chai.should();
+var request = require('supertest');
+var assert = chai.assert;
 
-describe('View Profile', function() {
+chai.use(chaiHttp);
+
+process.env.NODE_ENV = "test";
+
+const User = {
+    username: 'ahmed1hisham',
+    password: '123456789'
+};
+
+var token;
+
+var authenticatedUser = request.agent(server);
+before(function(done){
+    authenticatedUser
+        .post('/api/signIn')
+        .send(User)
+        .end(function(err, response){
+            response.should.have.status(200);
+            token = response.body.token;
+            done();
+        });
+});
+
+describe('Profile', function() {
 
 
-  describe('gets user info', function() {
-    it('should get user info', function(done){
-      var number = 9;
-      // request.get({url: '/api/profile'}, function(err, response, body){
-      //   expect(response.statusCode).to.equal(200);
-      //   console.log(body);
-      //   done();
-      // });
-      number.should.equal(9);
+  describe('Add as a parent', function() {
+    it('should add the correct child to the children of the selected parent', function(done){
+
+        authenticatedUser
+        .post('/api/profile/LinkAnotherParent/5ac4758a9ef2841987327718')
+        .send({
+            child: 'Lara'
+        })
+        .set( 'Authorization', token )
+        .end(function(err, res){
+            console.log(res.body);
+            should.exist(res);
+            res.should.have.status(200);
+        });
+      done();
     });
   });
+
 
 
 });
