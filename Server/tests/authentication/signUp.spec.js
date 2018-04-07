9a6d2584fa9771142d60355fb535196fc45527a0
Maqarios <maqarioswagdy@gmail.com>
2018-04-07 15:43:16 +0200

    feat: add base code for writing tests

---
+++ Server/tests/authentication/signUp.spec.js
@@ -0,0 +1,57 @@
+process.env.NODE_ENV = 'test';
+
+// --- Requirements --- //
+var app = require('../../app');
+var chai = require('chai');
+var config = require('../../api/config/config');
+var chaiHttp = require('chai-http');
+var mongoose = require('mongoose');
+var Mockgoose = require('mockgoose').Mockgoose;
+var User = require('../../api/models/User');
+// --- End of "Requirements" --- //
+
+// --- Dependancies --- //
+var expect = chai.expect;
+var should = chai.should();
+var mockgoose = new Mockgoose(mongoose);
+// --- End of "Dependancies" --- //
+
+// --- Middleware --- //
+chai.use(chaiHttp);
+// --- End of "Middleware" --- //
+
+describe('signUp', function () {
+
+    // --- Mockgoose Initiation --- //
+    before(function (done) {
+        mockgoose.prepareStorage().then(function () {
+            mongoose.connect(config.MONGO_URI, function () {
+                done();
+            });
+        });
+    });
+    // --- End of "Mockgoose Initiation" --- //
+
+    // --- Clearing Mockgoose --- //
+    beforeEach(function (done) {
+        mockgoose.helper.reset().then(function () {
+            done();
+        });
+    });
+    // --- End of "Clearing Mockgoose" --- //
+
+    // --- Tests --- //
+
+    /* Tests Goes Here */
+
+    // --- End of "Tests" --- //
+
+    // --- Mockgoose Termination --- //
+    after(function (done) {
+        mongoose.connection.close(function () {
+            done();
+        });
+    });
+    // --- End of "Mockgoose Termination" --- //
+
+});
