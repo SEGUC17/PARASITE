8fd5ad375bc4543fab369d75c810be9e4504e7f3
mariamkhmahran <mariamkhmahran@hotmail.com>
2018-04-08 15:29:31 +0200

    documenting

---
+++ Server/tests/profile/viewProfile.spec.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+var should = require('chai').should();
+
+describe('View Profile', function() {
+
+
+  describe('gets user', function() {
+    
+    
+    
+  });
+
+
+});
