5c805b0c179e35ae13b7b07c2fd4158aa3ccafce
--unset <omarelsebai3@yahoo.com>
2018-04-08 20:52:02 +0200

    test: done all schedule editting tests Note: tests will not work in this current state as there are problems with child Signup, I talked with the authentication component and we got a fix, but I will not push it in this branch. These tests are based on that fix so they will work when we merge.

--- Server/api/controllers/ScheduleController.js
+++ Server/api/controllers/ScheduleController.js
@@ -29,34 +29,47 @@ module.exports.updateSchedule = function (req, res, next) {
     var indexChild = req.user.children.indexOf(req.params.username);
     if (indexChild >= 0 ||
         (req.params.username === req.user.username && !req.user.isChild)) {
-    User.findOneAndUpdate(
-        { username: req.params.username },
-         { $set: { 'schedule': req.body } },
-     function (err, user) {
-        if (err) {
-            return next(err);
-        }
-        if (!user) {
-        return res.status(404).json({
-            data: null,
-            err: 'User not found.',
-            msg: null
-        });
-    }
+            var valid = Array.isArray(req.body);
+            if (valid) {
+                for (var index = 0; index < req.body.length; index += 1) {
+                    valid = valid && req.body[index].title &&
+                     req.body[index].start;
+                }
+            }
+            if (!valid) {
+                return res.status(422).json({
+                    data: null,
+                    err: 'Invalid Schedule',
+                    msg: null
+                });
+            }
+            User.findOneAndUpdate(
+            { username: req.params.username },
+            { $set: { 'schedule': req.body } }, { new: true },
+            function (err, user) {
+                if (err) {
+                    return next(err);
+                }
+                if (!user) {
+                    return res.status(404).json({
+                        data: null,
+                        err: 'User not found.',
+                        msg: null
+                    });
+                }
 
-        user.schedule = req.body;
 
-        return res.status(200).json({
-            data: user.schedule,
-            err: null,
-            msg: 'Schedule updated succesfully.'
-        });
-    }
-);
+                return res.status(200).json({
+                    data: user.schedule,
+                    err: null,
+                    msg: 'Schedule updated succesfully.'
+                });
+            }
+        );
     } else {
         return res.status(401).json({
             data: null,
-            err: 'Not authorized to edit user\'s Schedule ',
+            err: 'Not authorized to edit user\'s Schedule',
             msg: null
         });
     }
--- Server/tests/schedule/updatePersonalSchedule.spec.js
+++ Server/tests/schedule/updatePersonalSchedule.spec.js
@@ -35,6 +35,17 @@ var janeDoe = new User({
     phone: '123',
     username: 'jane'
 });
+var johnny = new User({
+    address: 'Johnny Address Sample',
+    birthdate: '1/1/1980',
+    email: 'johnny@gmail.com',
+    firstName: 'Johnny',
+    isTeacher: true,
+    lastName: 'Doe',
+    password: 'JohnPasSWorD',
+    phone: '123',
+    username: 'johnny'
+});
 var aCalendarEvent = {
     actions: [],
     color: {
@@ -77,19 +88,20 @@ describe('updateSchedule', function() {
         chai.request(server).
         post('/api/signUp').
         send(johnDoe).
-        end(function(error, data) {
-            if (error) {
-                return console.log(error);
+        end(function(err1, data) {
+            if (err1) {
+                return console.log(err1);
             }
             chai.request(server).
             patch('/api/schedule/saveScheduleChanges/' + johnDoe.username).
             set('Authorization', data.body.token).
             send([aCalendarEvent]).
-             end(function(err, res) {
-                 if (err) {
-                     return console.log(err);
+             end(function(err2, res) {
+                 if (err2) {
+                     return console.log(err2);
                  }
                  res.should.have.status(200);
+                 should.not.exist(res.body.err);
                  res.body.data.should.be.a('array');
                  res.body.data[0].should.be.a('object');
                  res.body.data[0].should.have.property('title');
@@ -102,8 +114,120 @@ describe('updateSchedule', function() {
              });
         });
     });
-    it('should PATCH user\'s child\'s personal schedule');
-    it('should NOT PATCH child\'s own personal schedule');
+    it('should PATCH user\'s child\'s personal schedule', function(done) {
+        chai.request(server).
+        post('/api/signUp').
+        send(johnDoe).
+        end(function(error, data) {
+            if (error) {
+                return console.log(error);
+            }
+            chai.request(server).
+            post('/api/childsignup').
+            set('Authorization', data.body.token).
+            send(johnny).
+             end(function(errr, ress) {
+                 if (errr) {
+                     return console.log(errr);
+                 }
+                 ress.should.have.status(201);
+                 chai.request(server).
+            patch('/api/schedule/saveScheduleChanges/' + johnny.username).
+            set('Authorization', data.body.token).
+            send([aCalendarEvent]).
+             end(function(err, res) {
+                 if (err) {
+                     return console.log(err);
+                 }
+                 res.should.have.status(200);
+                 should.not.exist(res.body.err);
+                 res.body.data.should.be.a('array');
+                 res.body.data[0].should.be.a('object');
+                 res.body.data[0].should.have.property('title');
+                 res.body.data[0].should.have.property('start');
+                 res.body.data[0].should.have.property('end');
+                 res.body.data[0].title.should.equal(aCalendarEvent.title);
+                 res.body.data[0].start.should.equal(aCalendarEvent.start);
+                 res.body.data[0].end.should.equal(aCalendarEvent.end);
+                 done();
+             });
+             });
+        });
+    });
+    it('should NOT PATCH child\'s own personal schedule', function(done) {
+        chai.request(server).
+        post('/api/signUp').
+        send(johnDoe).
+        end(function(error, data) {
+            if (error) {
+                return console.log(error);
+            }
+            chai.request(server).
+            post('/api/childsignup').
+            set('Authorization', data.body.token).
+            send(johnny).
+             end(function(errr, ress) {
+                chai.request(server).
+                post('/api/signIn').
+                send({
+                     'password': johnny.password,
+                     'username': johnny.username
+                    }).
+                end(function(signinError, siginData) {
+                    if (error) {
+                        return console.log(error);
+                    }
+                    console.log(siginData.body);
+                    chai.request(server).
+                    patch('/api/schedule/saveScheduleChanges/' + johnny.username).
+                    set('Authorization', siginData.body.token).
+                    send([aCalendarEvent]).
+                     end(function(err, res) {
+                         if (err) {
+                             return console.log(err);
+                         }
+                         res.should.have.status(401);
+                         res.body.err.should.be.a('string');
+                         res.body.err.should.
+                            equal('Not authorized to edit user\'s Schedule');
+                         should.not.exist(res.body.data);
+                         should.not.exist(res.body.msg);
+                         done();
+                     });
+                });
+             });
+        });
+    });
     it('should NOT PATCH another user\'s personal' +
-     'schedule who isn\'t the user\'s child');
+     'schedule who isn\'t the user\'s child', function(done) {
+        chai.request(server).
+        post('/api/signUp').
+        send(janeDoe).
+        end(function(errorr, dataa) {
+        chai.request(server).
+        post('/api/signUp').
+        send(johnDoe).
+        end(function(error, data) {
+            if (error) {
+                return console.log(error);
+            }
+            chai.request(server).
+            patch('/api/schedule/saveScheduleChanges/' + janeDoe.username).
+            set('Authorization', data.body.token).
+            send([aCalendarEvent]).
+             end(function(err, res) {
+                 if (err) {
+                     return console.log(err);
+                 }
+                 res.should.have.status(401);
+                 res.body.err.should.be.a('string');
+                 res.body.err.should.
+                    equal('Not authorized to edit user\'s Schedule');
+                 should.not.exist(res.body.data);
+                 should.not.exist(res.body.msg);
+                 done();
+             });
+        });
+    });
+});
 });
