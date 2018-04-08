5aee5b45ccc0097a045ccb166beb1f47551781f8
Wessam Abdelbary Ali <wessam611@live.com>
2018-04-08 03:57:53 +0200

    feat: get activities admin unstable

--- Server/tests/activities/viewActivities.spec.js
+++ Server/tests/activities/viewActivities.spec.js
@@ -10,6 +10,8 @@ var chaiHttp = require('chai-http');
 var mongoose = require('mongoose');
 var Mockgoose = require('mockgoose').Mockgoose;
 var Activity = mongoose.model('Activity');
+var User = mongoose.model('User');
+var jwt = require('jsonwebtoken');
 // --- End of 'Requirements' --- //
 
 // --- Dependancies --- //
@@ -22,12 +24,14 @@ var mockgoose = new Mockgoose(mongoose);
 chai.use(chaiHttp);
 // --- End of 'Middleware' --- //
 
+var adminUser = null;
 
 describe('Activities', function () {
     // --- Mockgoose Initiation --- //
     before(function (done) {
         mockgoose.prepareStorage().then(function () {
             mongoose.connect(config.MONGO_URI, function () {
+                
                 done();
             });
         });
@@ -37,13 +41,6 @@ describe('Activities', function () {
     // --- Clearing Mockgoose --- //
     beforeEach(function (done) {
         mockgoose.helper.reset().then(function () {
-            done();
-        });
-    });
-    // --- End of 'Clearing Mockgoose' --- //
-
-    describe('/GET activities', function () {
-        it('it should GET verified activities', function (done) {
             Activity.create({
                 creator: 'username',
                 name: 'activity1',
@@ -71,15 +68,35 @@ describe('Activities', function () {
                 status: 'verified',
                 price: 50
             });
+            User.create({
+                birthdate: Date.now(),
+                email: 'test@email.com',
+                firstName: 'firstname',
+                isAdmin: true,
+                lastName: 'lastname',
+                password: 'password',
+                phone: '0111111111',
+                username: 'username'
+            }, function(err, user) {
+                if (err) {
+                    console.log(err);
+                }
+                adminUser = user;
+            });
+            done();
+        });
+    });
+    // --- End of 'Clearing Mockgoose' --- //
+
+    describe('/GET activities unauthenticated', function () {
+        it('it should GET verified activities', function (done) {
             chai.request(app).get('/api/activities').
                 end(function (err, res) {
-                    // testing get activities for unverified user
                     if (err) {
                         console.log(err);
                     }
                     res.should.have.status(200);
-                    console.log(res.body.data);
-                    expect(res.body.data.total).to.equal(1);
+                    expect(res.body.data.docs.length).to.equal(1);
                     expect(res.body.data).to.have.ownProperty('page');
                     expect(res.body.data).to.have.ownProperty('pages');
                     expect(res.body.data).to.have.ownProperty('limit');
@@ -93,6 +110,29 @@ describe('Activities', function () {
         });
     });
 
+    describe('/GET activities authenticated admin', function () {
+        it('it should GET all activities', function (done) {
+            var token = 'JWT ' + jwt.sign(
+                { 'id': adminUser._id },
+                config.SECRET,
+                { expiresIn: '12h' }
+            );
+            chai.request(app).get('/api/activities').
+                set('Authorization', token).
+                end(function (err, res) {
+                    // testing get activities for unverified user
+                    if (err) {
+                        console.log(err);
+                    }
+                    res.should.have.status(200);
+                    expect(res.body.data.docs.length).to.equal(3);
+                    expect(res.body.data).to.have.ownProperty('page');
+                    expect(res.body.data).to.have.ownProperty('pages');
+                    expect(res.body.data).to.have.ownProperty('limit');
+                    done();
+                });
+        });
+    });
 
     // --- Mockgoose Termination --- //
     after(function (done) {
