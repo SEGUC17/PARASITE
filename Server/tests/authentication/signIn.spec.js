c698cad4cd3aeead2958901f6a54f33a4944df53
Maqarios <maqarioswagdy@gmail.com>
2018-04-09 15:49:55 +0200

    test: User Entered Valid Data (Username Has Space)!

--- Server/tests/authentication/signIn.spec.js
+++ Server/tests/authentication/signIn.spec.js
@@ -219,7 +219,19 @@ describe('signIn', function () {
                     });
             });
         });
-        it('User Entered Valid Data (Username Has Space)!');
+        it('User Entered Valid Data (Username Has Space)!', function(done) {
+            var self = this;
+            User.create(this.johnDoe, function(err) {
+                chai.request(app).
+                    post(path).
+                    send({'username': '  ' + self.johnDoe.username + '  ', 'password': self.johnDoe.password}).
+                    end(function(err, res) {
+                        res.should.have.status(200);
+                        res.body.should.have.property('msg').eql('Sign In Is Successful!');
+                        done();
+                    });
+            });
+        });
         it('Token Is Sent After Signning In!');
         it('Token Expires In 12 Hours!');
     });
