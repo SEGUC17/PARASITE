48f32da00437be5569e8dd24c2f8ab45a55afa15
Maqarios <maqarioswagdy@gmail.com>
2018-04-10 13:28:38 +0200

    test: "password" Attribute Is Requested!

--- Server/tests/authentication/getAnotherUserData.spec.js
+++ Server/tests/authentication/getAnotherUserData.spec.js
@@ -247,7 +247,20 @@ describe('getAnotherUserData', function () {
                 done();
             });
     });
-    it('"password" Attribute Is Requested!');
+    it('"password" Attribute Is Requested!', function(done) {
+        this.userDataColumns.push('password');
+        var self = this;
+        chai.request(app).
+            post(path + '  ' + this.janeDoe.username + '  ').
+            send(this.userDataColumns).
+            set('Authorization', this.token).
+            end(function (err, res) {
+                res.should.have.status(200);
+                res.body.should.have.property('data');
+                res.body.data.should.not.have.property('password');
+                done();
+            });
+    });
     it('"Non-Admin" Requesting "schdule"!');
     it('"Non-Admin" Requesting "studyPlans"!');
     it('"Non-Parent" Requesting "schdule"!');
