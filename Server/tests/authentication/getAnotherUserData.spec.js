6341f3f29cb60cafc401c078f75599a4a84a268d
Maqarios <maqarioswagdy@gmail.com>
2018-04-10 13:46:45 +0200

    test: "Non-Parent" Requesting "schedule"!

--- Server/tests/authentication/getAnotherUserData.spec.js
+++ Server/tests/authentication/getAnotherUserData.spec.js
@@ -309,7 +309,25 @@ describe('getAnotherUserData', function () {
                 done();
             });
     });
-    it('"Non-Parent" Requesting "schedule"!');
+    it('"Non-Parent" Requesting "schedule"!', function (done) {
+        this.userDataColumns.push('schedule');
+        var self = this;
+        chai.request(app).
+            post(path + '  ' + this.janeDoe.username + '  ').
+            send(this.userDataColumns).
+            set('Authorization', this.token).
+            end(function (err, res) {
+                res.should.have.status(200);
+                res.body.should.have.property('data');
+                res.body.data.should.not.have.property('schedule');
+                for (var index = 0; index < self.userDataColumns.length; index += 1) {
+                    if (self.userDataColumns[index] !== 'schedule') {
+                        res.body.data.should.have.property(self.userDataColumns[index]).eql(self.janeDoe[self.userDataColumns[index]]);
+                    }
+                }
+                done();
+            });
+    });
     it('"Non-Parent" Requesting "studyPlans"!');
     it('"Admin" Requesting "schedule"!');
     it('"Admin" Requesting "studyPlans"!');
