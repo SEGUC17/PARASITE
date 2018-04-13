365a0704f5869c7124ce4f5f75895b5afa07194c
Wessam Abdelbary Ali <wessam611@live.com>
2018-04-13 12:42:22 +0200

    test: basic test for comment detail

---
+++ Server/tests/activities/viewCommentDetailActivities.spec.js
@@ -0,0 +1,238 @@
+/* eslint-disable sort-keys */
+/* eslint-disable guard-for-in */
+/*eslint max-statements: ["error", 20]*/
+/* eslint multiline-comment-style: ["error", "starred-block"] */
+
+// --- Requirements --- //
+var app = require('../../app');
+var chai = require('chai');
+var config = require('../../api/config/config');
+var chaiHttp = require('chai-http');
+var mongoose = require('mongoose');
+var Mockgoose = require('mockgoose').Mockgoose;
+var Activity = mongoose.model('Activity');
+var User = mongoose.model('User');
+var jwt = require('jsonwebtoken');
+// --- End of 'Requirements' --- //
+
+// --- Dependancies --- //
+var expect = chai.expect;
+var mockgoose = new Mockgoose(mongoose);
+// --- End of 'Dependancies' --- //
+
+// --- Middleware --- //
+chai.use(chaiHttp);
+// --- End of 'Middleware' --- //
+
+// Objects variables for testing
+var normalUser = null;
+var adminUser = null;
+var rejectedActivity = null;
+var creatorUser = null;
+var pendingActivity = null;
+var verifiedActivity = null;
+
+var commentBody = null;
+
+describe('Activities Comments creation', function () {
+
+    /*
+     * Tests for GET Activity both list and detail
+     *
+     * All users can view a verified activity in list or in detail
+     * While pending and rejected activities can be viewed by admin
+     * in list and in detail and their creator in detail only.
+     *
+     * @author: Wessam
+     */
+
+    // --- Mockgoose Initiation --- //
+    before(function (done) {
+        mockgoose.prepareStorage().then(function () {
+            mongoose.connect(config.MONGO_URI, function () {
+
+                done();
+            });
+        });
+    });
+    // --- End of 'Mockgoose Initiation' --- //
+
+    // --- Clearing Mockgoose --- //
+    beforeEach(function (done) {
+        mockgoose.helper.reset().then(function () {
+            // Creating data for testing
+            commentBody = { text: 'comment test text' };
+            Activity.create({
+                creator: 'username',
+                name: 'activity1',
+                description: 'activity1 des',
+                fromDateTime: Date.now(),
+                toDateTime: Date.now() + 5,
+                status: 'pending',
+                price: 50
+            }, function (err, activity) {
+                if (err) {
+                    console.log(err);
+                }
+                pendingActivity = activity;
+            });
+            Activity.create({
+                creator: 'username',
+                name: 'activity2',
+                description: 'activity2 des',
+                fromDateTime: Date.now(),
+                toDateTime: Date.now() + 5,
+                status: 'rejected',
+                price: 50
+            }, function (err, activity) {
+                if (err) {
+                    console.log(err);
+                }
+                rejectedActivity = activity;
+            });
+            Activity.create({
+                creator: 'username',
+                name: 'activity3',
+                description: 'activity3 des',
+                fromDateTime: Date.now(),
+                toDateTime: Date.now() + 5,
+                status: 'verified',
+                discussion: [
+                    {
+                        creator: 'dummyUser',
+                        text: 'comment text',
+                        replies: [
+                            {
+                                creator: 'dummyUser',
+                                text: 'reply text'
+                            }
+                        ]
+                    },
+                    {
+                        creator: 'dummyUser',
+                        text: 'comment text2',
+                        replies: [
+                            {
+                                creator: 'dummyUser',
+                                text: 'reply1 text2'
+                            },
+                            {
+                                creator: 'dummyUser',
+                                text: 'reply2 text2'
+                            }
+                        ]
+                    },
+                    {
+                        creator: 'dummyUser',
+                        text: 'comment text3',
+                        replies: [
+                            {
+                                creator: 'dummyUser',
+                                text: 'reply text3'
+                            }
+                        ]
+                    }
+                ],
+                price: 50
+            }, function (err, activity) {
+                if (err) {
+                    console.log(err);
+                }
+                verifiedActivity = activity;
+            });
+            User.create({
+                birthdate: Date.now(),
+                email: 'test0@email.com',
+                firstName: 'firstname',
+                isAdmin: false,
+                lastName: 'lastname',
+                password: 'password',
+                phone: '0111111111',
+                username: 'normalusername'
+            }, function (err, user) {
+                if (err) {
+                    console.log(err);
+                }
+                normalUser = user;
+                User.create({
+                    birthdate: Date.now(),
+                    email: 'test1@email.com',
+                    firstName: 'firstname',
+                    isAdmin: true,
+                    lastName: 'lastname',
+                    password: 'password',
+                    phone: '0111111111',
+                    username: 'adminusername'
+                }, function (err2, user2) {
+                    if (err) {
+                        console.log(err2);
+                    }
+                    adminUser = user2;
+                    User.create({
+                        birthdate: Date.now(),
+                        email: 'test1@email.com',
+                        firstName: 'firstname',
+                        isAdmin: true,
+                        lastName: 'lastname',
+                        password: 'password',
+                        phone: '0111111111',
+                        username: 'username'
+                    }, function (err3, user3) {
+                        if (err) {
+                            console.log(err3);
+                        }
+                        creatorUser = user3;
+                        done();
+                    });
+                });
+            });
+        });
+    });
+    // --- End of 'Clearing Mockgoose' --- //
+
+    describe('/Commenting on activities', function () {
+
+        /*
+         * Tests for viewing on activity's comment
+         *
+         * @author: Wessam
+         */
+        it('it should comment on the activity', function (done) {
+            chai.request(app).
+                post('/api/activities/' +
+                 verifiedActivity._id +
+                 '/viewComment/' +
+                 verifiedActivity.discussion[0]._id).
+                send(commentBody).
+                end(function (err, res) {
+                    // testing get activities for unverified user
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.log(verifiedActivity.discussion[0]);
+                    res.should.have.status(200);
+                    expect(res.body.data).to.have.ownProperty('text');
+                    expect(res.body.data.text).
+                        to.equal(verifiedActivity.discussion[0].text);
+                    expect(res.body.data).to.have.ownProperty('replies');
+                    expect(res.body.data.replies.length).
+                        to.equal(verifiedActivity.discussion[0].replies.length);
+                    done();
+                });
+        });
+    });
+    // --- Clearing Mockgoose --- //
+    after(function (done) {
+        mockgoose.helper.reset().then(function () {
+            done();
+        });
+    });
+    // --- End of "Clearing Mockgoose" --- //
+
+    // --- Mockgoose Termination --- //
+    after(function (done) {
+        mongoose.connection.close(function () {
+            done();
+        });
+    });
+});
