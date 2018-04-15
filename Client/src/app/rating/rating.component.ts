0b8b1d1c60ee0be520dbc3eceea6eb50dcc0cb8c
Reda Ramadan <king2sairos2013@gmail.com>
2018-04-15 02:58:14 +0200

    feat: Add basic inputs to star rating component

--- Client/src/app/rating/rating.component.ts
+++ Client/src/app/rating/rating.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { RatingService } from '../rating.service';
-
+import { UserRating } from './rating';
 @Component({
   selector: 'app-rating',
   templateUrl: './rating.component.html',
   styleUrls: ['./rating.component..scss']
 })
 export class RatingComponent implements OnInit {
-
   constructor(private ratingService: RatingService) { }
+  // type of objects to be rated can only be
+  // one of the three following types
+  @Input() type: 'content' | 'studyPlan' | 'product';
+  // the ID of the object that you are rating.
+  @Input() ratedID: string;
+  // input rating in the form of numbers
+  @Input() rating: number;
 
+  public userRating: UserRating = {
+    type: this.type,
+    ratedID: this.ratedID,
+    rating: this.rating
+  };
   ngOnInit() {
   }
+
 
 }
