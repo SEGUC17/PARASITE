ee4df1fa55ac3493b9cb66152801f4e8a2c3158b
Reda Ramadan <king2sairos2013@gmail.com>
2018-04-22 12:17:58 +0200

    feat: Add category create component layout

---
+++ Client/src/app/admin/category-management/category-create/category-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-create',
+  templateUrl: './category-create.component.html',
+  styleUrls: ['./category-create.component.scss']
+})
+export class CategoryCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
