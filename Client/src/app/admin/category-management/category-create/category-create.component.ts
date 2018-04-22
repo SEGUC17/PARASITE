11fe63563583cf15c29483f7a148555f65723de3
Reda Ramadan <king2sairos2013@gmail.com>
2018-04-22 17:30:04 +0200

    feat: Finalize category create service connection

--- Client/src/app/admin/category-management/category-create/category-create.component.ts
+++ Client/src/app/admin/category-management/category-create/category-create.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AdminService } from '../../../admin.service';
+import { Category } from '../../../content/category';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-category-create',
   templateUrl: './category-create.component.html',
   styleUrls: ['./category-create.component.scss']
 })
 export class CategoryCreateComponent implements OnInit {
-
-  constructor() { }
+  category: any = {
+    category: '',
+    iconLink: ''
+  };
+  constructor(private adminService: AdminService, private toasterService: ToastrService) { }
 
   ngOnInit() {
   }
 
+  createCategory() {
+    const self = this;
+    this.adminService.createCategory(this.category).subscribe(function (res) {
+      if (!res) {
+        return;
+      }
+      self.toasterService.success('The category ' + res.data.name +
+        ' was created successfully', 'success');
+    });
+  }
 }
