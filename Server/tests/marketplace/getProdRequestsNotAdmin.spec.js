724e721ab5d93fa6d1c755098884f205b391f55c
JARVIS\Sinclair <exclass9.24@gmail.com>
2018-04-24 00:36:08 +0200

    chore: modified my tests for email verification

--- Server/tests/marketplace/getProdRequestsNotAdmin.spec.js
+++ Server/tests/marketplace/getProdRequestsNotAdmin.spec.js
@@ -4,10 +4,11 @@
 var mongoose = require('mongoose');
 var chai = require('chai');
 var server = require('../../app');
-// import your schema here, like this:
 var prodRequests = mongoose.model('ProductRequest');
+var User = mongoose.model('User');
 var chaiHttp = require('chai-http');
 var expect = require('chai').expect;
+var should = require('chai').should();
 
 chai.use(chaiHttp);
 
@@ -16,16 +17,17 @@ var Mockgoose = require('mockgoose').Mockgoose;
 var mockgoose = new Mockgoose(mongoose);
 
 // user for authentication
-var user = {
+var user = new User({
     birthdate: '1/1/1980',
     email: 'omar@omar.omar',
-    firstName: 'omar',
-    isAdmin: true,
+    firstName: 'Omar',
+    isAdmin: false,
+    isEmailVerified: true,
     lastName: 'Elkilany',
     password: '123456789',
     phone: '0112345677',
     username: 'omar'
-};
+});
 // authenticated token
 var token = null;
 
@@ -35,18 +37,33 @@ describe('GetProdRequestsAsNonAdmin', function () {
     before(function (done) {
         mockgoose.prepareStorage().then(function () {
             mongoose.connect(config.MONGO_URI, function () {
-                done();
+                mockgoose.helper.reset().then(function () {
+                    user.save(function (err) {
+                        if (err) {
+                            throw err;
+                        }
+
+                        chai.request(server).
+                            post('/api/signIn').
+                            send({
+                                'password': '123456789',
+                                'username': 'omar'
+                            }).
+                            end(function (err2, response) {
+                                if (err2) {
+                                    return console.log(err2);
+                                }
+                                response.should.have.status(200);
+                                token = response.body.token;
+                                done();
+                            });
+                    });
+                });
             });
         });
     });
     // --- End of "Mockgoose Initiation" --- //
-    // --- Clearing Mockgoose --- //
-    beforeEach(function (done) {
-        mockgoose.helper.reset().then(function () {
-            done();
-        });
-    });
-    // --- End of "Clearing Mockgoose" --- //
+
     it('It should NOT GET product requests from the server', function (done) {
         var prodReqTest = new prodRequests({
             acquiringType: 'sell',
@@ -57,39 +74,29 @@ describe('GetProdRequestsAsNonAdmin', function () {
             seller: 'omar'
         });
 
-        //sign up
-        chai.request(server).
-            post('/api/signUp').
-            send(user).
-            end(function (err, response) {
-                if (err) {
-                    return console.log(err);
-                }
-                response.should.have.status(201);
-                token = response.body.token;
-                // save your document with a call to save
-                prodReqTest.save(function (err) {
-                    if (err) {
-                        return console.log(err);
+        prodReqTest.save(function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            chai.request(server).
+                get('/api/productrequest/getRequests').
+                set('Authorization', token).
+                end(function (error, res) {
+                    if (error) {
+                        return console.log(error);
                     }
-                    chai.request(server).
-                        get('/api/productrequest/getRequests').
-                        set('Authorization', token).
-                        end(function (error, res) {
-                            if (error) {
-                                return console.log(error);
-                            }
-                            expect(res).to.have.status(403);
-                            res.body.err.should.be.equal('You are not an admin to do that.');
-                            done();
-                        });
+                    expect(res).to.have.status(403);
+                    res.body.err.should.be.equal('You are not an admin to do that.');
+                    done();
                 });
-            });
+        });
     });
     // --- Mockgoose Termination --- //
     after(function (done) {
-        mongoose.connection.close(function () {
-            done();
+        mockgoose.helper.reset().then(function () {
+            mongoose.connection.close(function () {
+                done();
+            });
         });
     });
     // --- End of "Mockgoose Termination" --- //
