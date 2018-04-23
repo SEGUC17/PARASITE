3dc8577e4d7f1450da8ce94dbe0f6522c318589e
Maqarios <maqarioswagdy@gmail.com>
2018-04-23 20:38:03 +0200

    feat: add sign up part 1

--- Client/src/app/auth/sign-up/sign-up.component.ts
+++ Client/src/app/auth/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { User } from '../user';
+import { cities } from '../../variables';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,11 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
+  public user: User = {
+    username: '',
+    password: '',
+    confirmPassword: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    birthdate: new Date(),
+    phone: '',
+    address: '',
+    isTeacher: false,
+  };
   year = (new Date()).getFullYear();
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  signUp() {
+    const self = this;
+    self.authService.signUp(this.user).subscribe(function (res) {
+      if (res.msg) {
+        alert(res.msg);
+      }
+    });
   }
 
 }
