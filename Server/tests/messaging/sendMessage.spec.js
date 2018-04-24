9391a138b507dc4cbc13cf5c4611af33690b57e6
Sarah Ayman <sarahshaker-97@live.com>
2018-04-24 12:12:46 +0200

    tests: fix tests

--- Server/tests/messaging/sendMessage.spec.js
+++ Server/tests/messaging/sendMessage.spec.js
@@ -23,16 +23,6 @@ var user = {
     username: 'sarah'
 };
 
-/*var user1 = new User({
-    birthdate: '2/6/1997',
-    email: 'test@gmail.com',
-    firstName: 'test',
-    lastName: 'test',
-    password: '123456789',
-    phone: '0174536975',
-    username: 'test'
-});*/
-
 // token for authentication
 var token = null;
 
@@ -59,53 +49,66 @@ describe('/POST message', function () {
 
     it('it should send a message to the specified recipient', function (done) {
 
-        // sign up and be authenticated
+        User.create(user, function(error) {
+            if (error) {
+                return done(error);
+            }
+
+        // sign in and be authenticated
         chai.request(server).
-            post('/api/signUp').
-            send(user).
-            end(function (err, response) {
-                if (err) {
-                    return console.log(err);
-                }
-                response.should.have.status(201);
-                token = response.body.token;
+        post('/api/signIn').
+        send({
+            'password': user.password,
+            'username': user.username
+        }).
+        end(function (err, response) {
+            if (err) {
+                return console.log(err);
+            }
+            response.should.have.status(200);
+            token = response.body.token;
 
-                // get username of logged in user
-                chai.request(server).post('/api/userData').
-                    send(['username']).
-                    set('Authorization', token).
-                    end(function (err, result) {
-                        if (err) {
-                            return console.log(err);
-                        }
-                        result.should.have.status(200);
-                        var message = {
-                            body: 'hi',
-                            recipient: 'test',
-                            sender: result.body.data.username
-                        };
+            // get username of logged in user
+            chai.request(server).post('/api/userData').
+                send(['username']).
+                set('Authorization', token).
+                end(function (errorr, result) {
+                    if (errorr) {
+                        return console.log(errorr);
+                    }
+                    result.should.have.status(200);
+                    var message = {
+                        body: 'hi',
+                        recipient: 'test',
+                        sender: result.body.data.username
+                    };
 
-                        // send post request
-                        chai.request(server).
-                            post('/api/message/sendMessage').
-                            send(message).
-                            set('Authorization', token).
-                            end(function (error, res) {
-                                if (error) {
-                                    return console.log(error);
-                                }
-                                // console.log(res.body.data);
-                                res.should.have.status(200);
-                                res.body.data.should.be.a('Object');
-                                res.body.data.should.have.property('sender').eql(result.body.data.username);
-                                res.body.data.should.have.property('recipient').eql(message.recipient);
-                                res.body.data.should.have.property('body').eql(message.body);
+                    // send post request
+                    chai.request(server).
+                        post('/api/message/sendMessage').
+                        send(message).
+                        set('Authorization', token).
+                        end(function (errr, res) {
+                            if (errr) {
+                                return console.log(errr);
+                            }
+                            // console.log(res.body.data);
+                            res.should.have.status(200);
+                            res.body.data.should.be.a('Object');
+                            res.body.data.should.have.property('sender').
+                            eql(result.body.data.username);
+                            res.body.data.should.have.property('recipient').
+                            eql(message.recipient);
+                            res.body.data.should.have.property('body').
+                            eql(message.body);
 
-                                done();
-                            });
-                    });
-            });
+                            done();
+                        });
+                });
+        });
     });
+});
+
     // --- Mockgoose Termination --- //
     after(function (done) {
         mongoose.connection.close(function () {
