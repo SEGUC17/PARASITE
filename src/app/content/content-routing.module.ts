94a51f54c4b8c54cd61c747bb40106dfebfc4166
Reda Ramadan <king2sairos2013@gmail.com>
2018-04-28 00:07:20 +0200

    feat: Update content module routing

--- src/app/content/content-routing.module.ts
+++ src/app/content/content-routing.module.ts
@@ -4,15 +4,35 @@ import { ContentEditComponent } from './content-edit/content-edit.component';
 import { ContentListViewComponent } from './content-list-view/content-list-view.component';
 import { ContentViewComponent } from './content-view/content-view.component';
 const routes = [
-  { path: 'content-edit', component: ContentEditComponent },
-  { path: 'content-edit/:id', component: ContentEditComponent },
-  { path: 'content-list-view', component: ContentListViewComponent },
-  { path: 'content-list-view/:tag', component: ContentListViewComponent },
-  { path: 'content-view/:id', component: ContentViewComponent }
+  {
+    path: 'view',
+    component: ContentListViewComponent,
+    children: [
+      {
+        path: ':tag',
+        component: ContentListViewComponent
+      },
+      {
+        path: ':id',
+        component: ContentViewComponent
+      }
+    ]
+  },
+  {
+    path: 'edit',
+    component: ContentEditComponent,
+    children: [
+      {
+        path: ':id',
+        component: ContentEditComponent,
+      }
+
+    ]
+  }
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forChild(routes)
   ],
   exports: [
     RouterModule
