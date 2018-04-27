0a8ec098cbc765608781cad74710bbe27cd0c92c
mariamkhmahran <mariamkhmahran@hotmail.com>
2018-04-28 00:54:26 +0200

    chore: a confirmation email with an ID is sent when psych request is accepted

--- api/utils/emails/email-verification.js
+++ api/utils/emails/email-verification.js
@@ -27,3 +27,20 @@ module.exports.send = function (email, link) {
         }
     });
 };
+
+module.exports.sendPsychID = function (email, id) {
+    var mailOptions = {
+        from: 'email-verification@nawwar.com',
+        to: email,
+        subject: 'Email Verification - Nawwar.com',
+        html: '<b><h2> Welcome to our family,</h2>'
+            + '<p> your request to add your information to our verified psychologists was accepted. Your ID: ' +
+            id + ' , keep this ID so you can use it later to edit or delete your information.' + '</p> <br>'
+            + '<span> best Regards,<span><br><span> Nawwar <span>'
+    };
+    transporter.sendMail(mailOptions, function (err, info) {
+        if (err) {
+            console.log(err);
+        }
+    });
+};
