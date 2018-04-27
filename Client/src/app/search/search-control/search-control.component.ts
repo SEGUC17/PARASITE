a03ed5486d5ad90959ebe08adb204a4ce151f077
Alfota <olfatm17@gmail.com>
2018-04-27 17:05:03 +0200

    fix: changing html to match template

--- Client/src/app/search/search-control/search-control.component.ts
+++ Client/src/app/search/search-control/search-control.component.ts
@@ -48,22 +48,22 @@ export class SearchControlComponent implements OnInit {
     console.log(self.tags[0] + ' ' + self.tags[1] + ' ' + self.tags[2] + ' ' + self.tags[3]);
   }
 // updating the tags used to search according to tab
-  getParents(tab: String) {
+  getParents() {
     const self = this;
-    switch (tab) {
-      case 'user':
-        this.tags[0] = this.selectedUsername;
-        break;
-      case 'eduL':
-        this.tags[1] = this.eduL;
-        break;
-      case 'eduS':
-        this.tags[2] = this.eduS;
-        break;
-      case 'loc':
-        this.tags[3] = this.loc;
-        break;
-    }
+
+      if (this.selectedUsername === 'user') {
+        this.tags[0] = this.searchKey;
+      }
+      if (this.eduL === 'eduL') {
+        this.tags[1] = this.searchKey;
+      }
+      if (this.eduS === 'eduS') {
+        this.tags[2] = this.searchKey;
+      }
+      if (this.loc === 'loc') {
+        this.tags[3] = this.searchKey;
+       }
+
 // updating search page according to new set of tags
     this.currPage = 1;
     this.getCurrPage();
@@ -73,6 +73,33 @@ export class SearchControlComponent implements OnInit {
   goToProfile(username: string) {
     this.searchService.viewProfile(username);
   }
+
+  // calculate the number of pages to display in pagination
+  getPaginationRange(): any {
+
+    let pageNumbers = [];
+    let counter = 1;
+
+    if (this.currPage < 3) {
+      // we are in page 1 or 2
+      while (counter < 6 && counter <= this.totPages) {
+        pageNumbers.push(counter);
+        counter += 1;
+      }
+    } else {
+      // we are in a page greater than 2
+      pageNumbers.push(this.currPage - 2);
+      pageNumbers.push(this.currPage - 1);
+      pageNumbers.push(this.currPage);
+      if (this.currPage + 1 <= this.totPages) {
+        pageNumbers.push(this.currPage + 1);
+      }
+      if (this.currPage + 2 <= this.totPages) {
+        pageNumbers.push(this.currPage + 2);
+      }
+    }
+    return pageNumbers;
+}
 // getting the page according to the index
   getPage(event): void {
 
@@ -91,6 +118,12 @@ export class SearchControlComponent implements OnInit {
         this.totParents = retreivedUsers.data.total;
     });
   }
+  changePage(pageNumber: number): void {
+
+    this.currPage = pageNumber;
+    window.scrollTo(0, 0);
+    this.getParents();
+}
 // initializing all the parameters to starter values
   ngOnInit() {
     this.currPage = 1;
