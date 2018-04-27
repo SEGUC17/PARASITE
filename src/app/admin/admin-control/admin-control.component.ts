c856fc9ae7b555a7f8063942304b458c121cf65e
Reda Ramadan <king2sairos2013@gmail.com>
2018-04-28 00:10:00 +0200

    feat: update admin control router links

--- src/app/admin/admin-control/admin-control.component.ts
+++ src/app/admin/admin-control/admin-control.component.ts
@@ -1,12 +1,5 @@
 import { OnInit, Input, Output, ViewChild, Component } from '@angular/core';
-import { ViewVerifiedContributerRequestsComponent } from '../view-verified-contributer-requests/view-verified-contributer-requests.component';
-import { ViewContentRequestsComponent } from '../../content/view-content-requests/view-content-requests.component';
-import { ViewUnverifiedActivitiesComponent } from '../view-unverified-activities/view-unverified-activities.component';
-import { AdminService } from '../../admin.service';
-import { Router } from '@angular/router';
-import { ViewProductRequestsComponent } from '../view-product-requests/view-product-requests.component';
-import { ViewPsychRequestsComponent } from '../view-psych-requests/view-psych-requests.component';
-import { PublishRequestsComponent } from '../../schedule/study-plan/publish-requests/publish-requests.component';
+import { ViewContentRequestsComponent } from '../view-content-requests/view-content-requests.component';
 @Component({
   selector: 'app-admin-control',
   templateUrl: './admin-control.component.html',
@@ -14,60 +7,42 @@ import { PublishRequestsComponent } from '../../schedule/study-plan/publish-requ
 })
 
 export class AdminControlComponent implements OnInit {
-
-  @ViewChild(ViewVerifiedContributerRequestsComponent) VcComponent;
-
-  @ViewChild(ViewContentRequestsComponent) _ResIReq;
-
-  @ViewChild(ViewProductRequestsComponent) ProdReqComponent;
-
-  @ViewChild(ViewPsychRequestsComponent) PsychReqComponent;
-
-  @ViewChild(PublishRequestsComponent) PubReqComponent;
-
-  hideVCRequest: any = 1;
-  hideContentReqs: any = 1;
-  hideProdReqs: any = 1;
-  hidePsychReqs: any = 1;
-  hideStudyPlanPublishReqs: any = 1;
-
-  constructor(private _adminService: AdminService,
-    private router: Router) { }
-
+  constructor() { }
+  links = [
+    {
+      name: 'Content Requests',
+      url: './content-req'
+    },
+    {
+      name: 'Category Management',
+      url: './category'
+    },
+    {
+      name: 'Product Requests',
+      url: './product-req'
+    },
+    {
+      name: 'Psychologist Requests',
+      url: './psych-req'
+    },
+    {
+      name: 'Publish Study Plan requests',
+      url: './publish-req'
+    },
+    {
+      name: 'Unverified Activities',
+      url: './unverified-activites'
+    },
+    {
+      name: 'Verified Contributor Requests',
+      url: './verified-contributor-req'
+    },
+    {
+      name: 'Reports',
+      url: './reports'
+    }
+  ];
   ngOnInit() {
 
-  }
-
-  // changing the visibility of the component
-  viewProdRequests() {
-    this.hideProdReqs = 1 - this.hideProdReqs;
-  }
-
-  goToResIReq() {
-    this.hideContentReqs = 1 - this.hideContentReqs;
-    this._ResIReq.viewPendingContReqs();
-  }
-
-  onSelectChange(event) {
-    if (event.index === 1) {
-      this._ResIReq.viewPendingContReqs();
-    }
-    if (event.index === 5) {
-      this.PubReqComponent.viewStudyPlanPublishReqs();
-    }
-  }
-
-  // changing the visibility of the component
-  goToPsychReq() {
-    this.hidePsychReqs = 1 - this.hidePsychReqs;
-  }
-
-  goToPubReq() {
-    this.hideStudyPlanPublishReqs = 1 - this.hideStudyPlanPublishReqs;
-    this.PubReqComponent.viewStudyPlanPublishReqs();
-  }
-  viewVCRequests() {
-    console.log('gonna hide the component');
-    this.hideVCRequest = 1 - this.hideVCRequest;  // changing the visibility of the component
   }
 }
