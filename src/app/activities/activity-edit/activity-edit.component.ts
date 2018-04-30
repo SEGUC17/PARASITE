96ee3b278b00d388706f8414e48d6b63bf9958d2
Heidi <Heidiiashraaf@gmail.com>
2018-05-01 01:22:54 +0200

    feat: translate edit activity

--- src/app/activities/activity-edit/activity-edit.component.ts
+++ src/app/activities/activity-edit/activity-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Inject} from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { TranslateService} from '@ngx-translate/core';
 @Component({
   selector: 'app-activity-edit',
   templateUrl: './activity-edit.component.html',
@@ -8,7 +9,7 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 })
 export class ActivityEditComponent implements OnInit {
 
-  constructor(
+  constructor(private translate: TranslateService,
     public dialogRef: MatDialogRef<ActivityEditComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) { } // Edit activity dialog
 
