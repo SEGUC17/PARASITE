18369e8bbd046fd060a21e48d293545a85dccf09
OmarElkilany <omar.alkilani23@gmail.com>
2018-05-01 14:47:30 +0200

    translate forgot password

--- src/app/auth/forgotPassword/forgotPassword.component.ts
+++ src/app/auth/forgotPassword/forgotPassword.component.ts
@@ -4,6 +4,7 @@ import { AuthService } from '../auth.service';
 import { MatButtonModule } from '@angular/material';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 
 @Component({
@@ -16,7 +17,7 @@ export class ForgotPasswordComponent implements OnInit {
   // ------ flags -----//
  email = '';
 
-  constructor(private _AuthService: AuthService, private _Location: Location) { }
+  constructor(private _AuthService: AuthService, private _Location: Location, private translate: TranslateService) { }
 
   ngOnInit() {
   }
