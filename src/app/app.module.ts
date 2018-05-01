80f6f744f5f863fc92dc519bb43cd3ab9e02abbf
Ahmed Hisham Rihan <ahmed11rihan@gmail.com>
2018-05-01 19:54:24 +0200

    fix: removed Profile Image uploader from App module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,7 +28,6 @@ import { environment } from '../environments/environment';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient } from '@angular/common/http';
-import { ProfileImageUploaderComponent } from './profile-image-uploader/profile-image-uploader.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -36,8 +35,7 @@ export function HttpLoaderFactory(http: HttpClient) {
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProfileImageUploaderComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
