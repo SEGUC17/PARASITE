c76cbf09d4df2646790731c756cca71b3cb5652d
Ahmed Hisham Rihan <ahmed11rihan@gmail.com>
2018-05-02 11:39:50 +0200

    fix: fixed banning users in admin control page

--- src/app/admin/view-reports/view-reports.component.ts
+++ src/app/admin/view-reports/view-reports.component.ts
@@ -83,7 +83,7 @@ export class ViewReportsComponent implements OnInit {
         if ((<HTMLInputElement>document.getElementById('bannedUsername')).value) {
 
             this._authService.getAnotherUserData(['_id'], username).subscribe((resp) => {
-                if (resp.data._id) {
+                if (resp.data) {
                     this.banResponse = true;
                 } else {
                     this.banResponse = false;
