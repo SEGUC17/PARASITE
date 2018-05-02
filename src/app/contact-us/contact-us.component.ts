84d0a0059dd5dc8587728c79581ba3a0012adfb8
Reda Ramadan <king2sairos2013@gmail.com>
2018-05-02 04:50:29 +0200

    fix: Update contact us component

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { MessageService } from '../messaging/messaging.service';
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from '../auth/auth.service';
+import { TranslateService } from '@ngx-translate/core';
 
 
 @Component({
@@ -15,9 +16,9 @@ import { AuthService } from '../auth/auth.service';
 
 export class ContactUsComponent implements OnInit {
   public msg: any = {
-  body: '',
-  recipient: '',
-  sender: ''
+    body: '',
+    recipient: '',
+    sender: ''
   };
   response: any;
 
@@ -28,41 +29,34 @@ export class ContactUsComponent implements OnInit {
 
 
   constructor(private _MessageService: MessageService,
-
     private _Toastr: ToastrService,
+    private _AuthService: AuthService, private translate: TranslateService) {
 
-    private _AuthService: AuthService) { }
+    const self = this;
+    const userDataColumns = ['username'];
+    this._AuthService.getUserData(userDataColumns).subscribe(function (response) {
+      self.user = true;
+    }, function (error) {
+      if (error.status === 401) {
+        self.visitor = true;
+      }
+    });
+  }
 
 
 
   ngOnInit() {
-    const self = this;
-    const userDataColumns = ['username'];
-    this._AuthService.getUserData(userDataColumns).subscribe(function(response) {
-      if (response.msg === 'User Is Not Signed In!') {
-          self.visitor = true;
-      }
-      if (!(response.msg === 'Data Retrieval Is Successful!')) {
-        self.visitor = true;
-
-      } else {
-        self.msg.sender = response.data.username;
-        self.user = true;
-      }
-    });
-
   }
   contact_us() {
-    // tslint:disable-next-line:max-line-length
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     const self = this;
     if (self.msg.body === '') {
       self._Toastr.warning('Cannot send an empty message');
-    }  else if (self.visitor === true && (!(re.test(self.msg.sender))))  {
+    } else if (self.visitor === true && (!(re.test(self.msg.sender)))) {
       self._Toastr.warning('You need to provide an email!');
     } else {
-      this._MessageService.contactus(self.msg).subscribe(function(res) {
+      this._MessageService.contactus(self.msg).subscribe(function (res) {
         self._Toastr.success(res.msg);
       });
     }
