be8ff134e9504107417126d741217b995b5dd697
OmarElkilany <omar.alkilani23@gmail.com>
2018-05-05 23:00:49 +0200

    toastr for feature access restrictions

--- src/app/feature-guard.service.ts
+++ src/app/feature-guard.service.ts
@@ -5,11 +5,14 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/of';
+import { ToastrService } from 'ngx-toastr';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable()
 export class FeatureGuardService implements CanActivate, CanLoad {
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router,
+    private toastrService: ToastrService, private translate: TranslateService) { }
 
   canActivate() {
     const self = this;
@@ -17,17 +20,20 @@ export class FeatureGuardService implements CanActivate, CanLoad {
     return this.authService.getUserData(['username']).map(function (user) {
       if (!user.data) {
         self.router.navigate(['/content/list']);
+        self.callToastr();
         return false;
       }
       if (user.data.username) {
         return true;
       } else {
         self.router.navigate(['/content/list']);
+        self.callToastr();
         return false;
       }
     }).catch(
       function (error) {
         self.router.navigate(['/content/list']);
+        self.callToastr();
         return Observable.of(false);
       }
     );
@@ -41,18 +47,30 @@ export class FeatureGuardService implements CanActivate, CanLoad {
     return this.authService.getUserData(['username']).map(function (user) {
       if (!user.data) {
         self.router.navigate(['/content/list']);
+        self.callToastr();
         return false;
       }
       if (user.data.username) {
         return true;
       } else {
         self.router.navigate(['/content/list']);
+        self.callToastr();
         return false;
       }
     }).catch(
       function (error) {
         self.router.navigate(['/content/list']);
+        self.callToastr();
         return Observable.of(false);
+      }
+    );
+  }
+
+  callToastr() {
+    const self = this;
+    this.translate.get('APP.SIGN_UP_FOR_FULL_ACCESS').subscribe(
+      function (message) {
+        self.toastrService.warning(message);
       }
     );
   }
