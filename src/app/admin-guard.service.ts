89b09e75ea9fb0ca51d9f93a64a26f817003d545
OmarElkilany <omar.alkilani23@gmail.com>
2018-05-05 23:13:43 +0200

    toastr for the brilliant admin. May god bless him/her

--- src/app/admin-guard.service.ts
+++ src/app/admin-guard.service.ts
@@ -5,11 +5,14 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/of';
+import { ToastrService } from 'ngx-toastr';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable()
 export class AdminGuardService implements CanActivate, CanLoad {
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router,
+    private toastrService: ToastrService, private translate: TranslateService) { }
 
   canActivate() {
     const self = this;
@@ -17,17 +20,20 @@ export class AdminGuardService implements CanActivate, CanLoad {
     return this.authService.getUserData(['isAdmin']).map(function (user) {
       if (!user.data) {
         self.router.navigate(['/content/list']);
+        self.callToastr();
         return false;
       }
       if (user.data.isAdmin) {
         return true;
       } else {
         self.router.navigate(['/content/list']);
+        self.callToastr();
         return false;
       }
     }).catch(
       function(error) {
         self.router.navigate(['/content/list']);
+        self.callToastr();
         return Observable.of(false);
       }
     );
@@ -39,18 +45,30 @@ export class AdminGuardService implements CanActivate, CanLoad {
     return this.authService.getUserData(['isAdmin']).map(function (user) {
       if (!user.data) {
         self.router.navigate(['/content/list']);
+        self.callToastr();
         return false;
       }
       if (user.data.isAdmin) {
         return true;
       } else {
         self.router.navigate(['/content/list']);
+        self.callToastr();
         return false;
       }
     }).catch(
       function(error) {
         self.router.navigate(['/content/list']);
+        self.callToastr();
         return Observable.of(false);
+      }
+    );
+  }
+
+  callToastr() {
+    const self = this;
+    this.translate.get('APP.SIGN_IN_ADMIN').subscribe(
+      function (message) {
+        self.toastrService.warning(message);
       }
     );
   }
