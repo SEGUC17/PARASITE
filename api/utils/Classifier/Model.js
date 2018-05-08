3ef0e1e6a2527515e4aff4bbe7d7bf196905b931
Maher <momaherg@gmail.com>
2018-05-08 15:44:05 +0200

    finished classifier

--- api/utils/Classifier/Model.js
+++ api/utils/Classifier/Model.js
@@ -11,8 +11,9 @@ module.exports.loadDict = function() {
       if (err) {
         return console.log(err);
       }
-      var dataset = [];
+      var datasetmain = [];
       var lines = data.split('\n');
+      // var batchSize = 10
       var totalDatasetLength = lines.length - (lines.length%10);
 
       console.log('loading the dictionary and the dataset...');
@@ -22,7 +23,7 @@ module.exports.loadDict = function() {
         var w = line[0];
         var t = line[1];
         var l = line[2];
-        dataset.push({
+        datasetmain.push({
           word: w,
           id: i,
           title: t,
@@ -33,8 +34,8 @@ module.exports.loadDict = function() {
 
       let datasetWords = [];
 
-      for(var i =0; i < dataset.length ; i++) {
-        datasetWords.push(dataset[i].word);
+      for(var i =0; i < datasetmain.length ; i++) {
+        datasetWords.push(datasetmain[i].word);
       }
 
       var Words = datasetWords.filter(function(elem, index, self) {
@@ -51,7 +52,7 @@ module.exports.loadDict = function() {
       }));
       classifier.add(tf.layers.dense({
         units: 100,
-        activation: 'relu',
+        activation: 'sigmoid',
         kernelInitializer: 'glorotUniform'
       }));
       classifier.add(tf.layers.dense({
@@ -61,53 +62,66 @@ module.exports.loadDict = function() {
       }));
 
       classifier.compile({
-        optimizer: tf.train.sgd(0.15),
+        optimizer: 'Adam',
         loss: 'meanSquaredError',
         metrics: ['accuracy']
       });
 
 
-      dataset = dataset.slice(0,200);
+      let dataset = datasetmain.slice(0,5000);
 
-      for(let k = 0; k< dataset.length; k= k+50) {
-        // (tf.tidy(() => {
+      for(let k = 0; k< dataset.length; k= k+200) {
+        let  X_train_tensor;
+        let Y_train_tensor
+        (tf.tidy(() => {
 
-          let batch = dataset.slice(k,k+50);
+          let batch = dataset.slice(k,k+200);
 
+          X_train_tensor = (tf.tidy(() => {
+            let X       = tf.oneHot(tf.tensor1d([Words.indexOf(batch[0].word)], 'int32'), Words.length).as1D();
+            let curr_X_ = tf.zeros([Words.length]);
+            return tf.concat([curr_X_, X,tf.tensor1d([batch[0].title], 'float32')], 0).flatten().as2D(1,X.shape[0] + curr_X_.shape[0] +1);
+          }));
 
-          let  X_train_tensor = tf.oneHot(tf.tensor1d([Words.indexOf(batch[0].word)], 'int32'), Words.length).as1D();
-          let curr_X_ = tf.zeros([Words.length]);
-          X_train_tensor = tf.concat([curr_X_, X_train_tensor,tf.tensor1d([batch[0].title], 'float32')], 0).flatten().as2D(1,X_train_tensor.shape[0] + curr_X_.shape[0] +1);
-
-          let Y_train_tensor = tf.tensor1d([batch[0].label[0]], 'float32').as2D(1,1);
+          Y_train_tensor = tf.tensor1d([batch[0].label[0]], 'float32').as2D(1,1);
 
           // making the X_train
           for (let i = 1; i < batch.length; i++) {
-            let curr_x = (tf.tidy(() => {
+            // console.log(tf.memory().numTensors + ' tensors in memory');
+
+            X_train_tensor = (tf.tidy(() => {
               const curr_X_tensor = tf.oneHot(tf.tensor1d([Words.indexOf(batch[i].word)], 'int32'), Words.length).as1D();
               curr_X_             = tf.oneHot(tf.tensor1d([Words.indexOf(batch[i-1].word)], 'int32'), Words.length).as1D();
-              return tf.concat([curr_X_,curr_X_tensor,tf.tensor1d([batch[i].title], 'float32')], 0).flatten();
+              let curr_x = tf.concat([curr_X_,curr_X_tensor,tf.tensor1d([batch[i].title], 'float32')], 0).flatten();
+              return tf.concat([X_train_tensor, curr_x.as2D(1,curr_x.shape[0])], 0);
             }));
-            X_train_tensor = tf.concat([X_train_tensor, curr_x.as2D(1,curr_x.shape[0])], 0);
-            let curr_y = (tf.tidy(() => {
-              return tf.tensor1d([batch[i].label[0]], 'float32');
+
+
+            // console.log(tf.memory().numTensors + ' tensors in memory');
+            Y_train_tensor =  (tf.tidy(() => {
+              let curr_y = tf.tensor1d([batch[i].label[0]], 'float32');
+              return tf.concat([Y_train_tensor, curr_y.as2D(1,curr_y.shape[0])], 0);
             }));
-            Y_train_tensor = tf.concat([Y_train_tensor, curr_y.as2D(1,curr_y.shape[0])], 0);
+
           }
-
-          console.log('fitting batch number ' + k + ' from ' + dataset.length);
-          classifier.fit( X_train_tensor, Y_train_tensor, {batchSize: 50, epochs: 10 });
-
-        // }));
+          // console.log(X_train_tensor.shape);
+          console.log('fitting batch number ' + k + ' from ' + dataset.length + ' with ' + tf.memory().numTensors + ' tensors in memory');
+          var promise = classifier.fit( X_train_tensor, Y_train_tensor, {batchSize: 20, epochs: 10 });
+          // X_train_tensor.dispose();
+          // Y_train_tensor.dispose();
+          promise.catch(function(error) {
+            // console.log('error in batch ' + k + error.message);
+          });
+        }));
 
       }
       //
-      console.log('Dictionary loaded Successfully')
+      console.log('Dictionary loaded Successfully with ' + tf.memory().numTensors + ' tensor in memory');
 
-      let predictions = [];
-      let Y_test   = [];
-
-      var testset = dataset.slice(0, 50);
+      let predictions = 0;
+      let Y_test   = 0;
+      let testset = datasetmain.slice();
+       testset = testset.slice(2000, 2300);
 
       var testsetWords = [];
 
@@ -116,63 +130,96 @@ module.exports.loadDict = function() {
       }
 
       // Testing set //////////////////////////////////////
-      for(let f = 0; f< testset.length; f= f+10) {
+      let batchSize = 50;
+      /// Initial testing //////////////////////////////////////////
+      let batch = testset.slice(0,batchSize);
+
+      let X_test_tensor = tf.oneHot(tf.tensor1d([Words.indexOf(batch[0].word)], 'int32'), Words.length).as1D();
+      let curr_X_test   = tf.zeros([Words.length]);
+      X_test_tensor     = tf.concat([curr_X_test,X_test_tensor,tf.tensor1d([batch[0].title], 'float32')], 0).flatten().as2D(1,X_test_tensor.shape[0] + curr_X_test.shape[0] +1);
+
+      let Y_test_tensor = tf.tensor1d([batch[0].label[0]], 'float32').as2D(1,1);
+
+      // making the X_test
+      console.log('loading TestSet ... ' + 0);
+      for (i = 1; i < batch.length; i++) {
+        let id = Words.indexOf(batch[i].word); let title = batch[i].title; let label = batch[i].label;
+        let curr_x;
+        let curr_X_;
+        if( id == -1 ){
+          curr_x = (tf.tidy(() => {
+            const curr_X_tensor = tf.zeros([Words.length]);
+            curr_X_             = tf.zeros([Words.length]);
+            return tf.concat([curr_X_, curr_X_tensor,tf.tensor1d([title], 'float32')], 0).flatten();
+          }));
+        }
+        else {
+          curr_x = (tf.tidy(() => {
+            const curr_X_tensor = tf.oneHot(tf.tensor1d([id], 'int32'), Words.length).as1D();
+            curr_X_             = tf.oneHot(tf.tensor1d([(Words.indexOf(batch[i-1].word))], 'int32'), Words.length).as1D();
+            return tf.concat([curr_X_, curr_X_tensor,tf.tensor1d([title], 'float32')], 0).flatten();
+          }));
+        }
+
+        X_test_tensor = tf.concat([X_test_tensor, curr_x.as2D(1,curr_x.shape[0])], 0);
+
+        var curr_y = (tf.tidy(() => {
+          return tf.tensor1d([batch[i].label], 'float32');
+        }));
+        Y_test_tensor = tf.concat([Y_test_tensor, curr_y.as2D(1,curr_y.shape[0])], 0);
+      }
+      let p = classifier.predict(X_test_tensor);
+      predictions = p;
+      Y_test = Y_test_tensor;
+
+      ////////////////////////////////////////////////////////
+      for(let f = batchSize; f< testset.length; f= f+batchSize) {
         tf.tidy(() => {
-        let batch = testset.slice(f,f+10);
+          let batch = testset.slice(f,f+batchSize);
 
-        let X_test_tensor = tf.oneHot(tf.tensor1d([Words.indexOf(batch[0].word)], 'int32'), Words.length).as1D();
-        let curr_X_test   = tf.zeros([Words.length]);
-        X_test_tensor     = tf.concat([curr_X_test,X_test_tensor,tf.tensor1d([batch[0].title], 'float32')], 0).flatten().as2D(1,X_test_tensor.shape[0] + curr_X_test.shape[0] +1);
+          let X_test_tensor = tf.oneHot(tf.tensor1d([Words.indexOf(batch[0].word)], 'int32'), Words.length).as1D();
+          let curr_X_test   = tf.zeros([Words.length]);
+          X_test_tensor     = tf.concat([curr_X_test,X_test_tensor,tf.tensor1d([batch[0].title], 'float32')], 0).flatten().as2D(1,X_test_tensor.shape[0] + curr_X_test.shape[0] +1);
 
-        let Y_test_tensor = tf.tensor1d([batch[0].label[0]], 'float32').as2D(1,1);
+          let Y_test_tensor = tf.tensor1d([batch[0].label[0]], 'float32').as2D(1,1);
 
-        // making the X_test
-        console.log('loading TestSet ... ' + testset.length);
-        for (i = 1; i < batch.length; i++) {
-          let id = Words.indexOf(batch[i].word); let title = batch[i].title; let label = batch[i].label;
-          let curr_x;
-          let curr_X_;
-          if( id == -1 ){
-            curr_x = (tf.tidy(() => {
-              const curr_X_tensor = tf.zeros([Words.length]);
-              curr_X_             = tf.zeros([Words.length]);
-              return tf.concat([curr_X_, curr_X_tensor,tf.tensor1d([title], 'float32')], 0).flatten();
-            }));
-          }
-          else {
+          // making the X_test
+          console.log('loading TestSet ... ' + f);
+          for (i = 1; i < batch.length; i++) {
+            let id = Words.indexOf(batch[i].word); let title = batch[i].title; let label = batch[i].label;
+            let curr_x;
+            let curr_X_;
             curr_x = (tf.tidy(() => {
               const curr_X_tensor = tf.oneHot(tf.tensor1d([id], 'int32'), Words.length).as1D();
               curr_X_             = tf.oneHot(tf.tensor1d([(Words.indexOf(batch[i-1].word))], 'int32'), Words.length).as1D();
               return tf.concat([curr_X_, curr_X_tensor,tf.tensor1d([title], 'float32')], 0).flatten();
             }));
-          }
+            X_test_tensor = tf.concat([X_test_tensor, curr_x.as2D(1,curr_x.shape[0])], 0);
 
-          X_test_tensor = tf.concat([X_test_tensor, curr_x.as2D(1,curr_x.shape[0])], 0);
-
-          var curr_y = (tf.tidy(() => {
-            return tf.tensor1d([batch[i].label], 'float32');
-          }));
-          Y_test_tensor = tf.concat([Y_test_tensor, curr_y.as2D(1,curr_y.shape[0])], 0);
+            var curr_y = (tf.tidy(() => {
+              return tf.tensor1d([batch[i].label], 'float32');
+            }));
+            Y_test_tensor = tf.concat([Y_test_tensor, curr_y.as2D(1,1)], 0);
           }
           let p = classifier.predict(X_test_tensor);
-          if(predictions == []) { predictions = p.as2D(1,p.shape[0]); } else {
-            predictions = tf.concat([predictions, p], 0)  ;
-          }
-          if(Y_test == []) { Y_test = Y_test_tensor.as2D(1,Y_test_tensor.shape[0]); } else {
-            Y_test = tf.concat([Y_test, Y_test_tensor], 0);
-          }
-          console.log('inside of Tidy ' + tf.memory().numTensors);
-
+          console.log((p.data()));
+          var equality = tf.equal(tf.round(p), Y_test_tensor);
+          var accuracy = tf.mean(equality);
+          console.log('The Accuracy of batch ' + f + ' is :');
+          accuracy.print();
         });
         console.log('predicting batch number ' + f + ' from ' + testset.length);
 
       }
-      console.log('Testset Loaded Successfully !!');
-      console.log('final ' + tf.memory().numTensors);
+
+      console.log('the Classifier is ready');
+
+      // console.log('Testset Loaded Successfully !!');
+      // console.log('final ' + tf.memory().numTensors);
 
       ///////////////////////////////////////////////////////
 
-      console.log(predictions);
+      // console.log(predictions);
 
       // let prediction = predictions[0].as2D(1,predictions[0].shape[0]);
       // let Y = Y_train[0].as2D(1,Y_train[0].shape[0]);
@@ -186,12 +233,12 @@ module.exports.loadDict = function() {
       // {
       //   Y = Y.concat([Y, Y_train[g]],0);
       // }
-      predictions.print();
-
-      var equality = tf.equal(tf.round(predictions), Y_train);
-      var accuracy = tf.mean(equality);
-      console.log('The Accuracy is');
-      accuracy.print();
+      // predictions.print();
+      //
+      // var equality = tf.equal(tf.round(predictions), Y_train);
+      // var accuracy = tf.mean(equality);
+      // console.log('The Accuracy is');
+      // accuracy.print();
 
     });
     return 0;
